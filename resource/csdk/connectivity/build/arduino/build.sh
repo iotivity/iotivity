eb87639154422ec01e8118bd827a95e4fc9a813b
Joseph Morrow <joseph.l.morrow@intel.com>
2015-02-02 19:07:30 -0500

    Changes to make Linux build with CA_INT flag specified.
    These changes also include some work to ensure better global cleanup
    in makefiles. Some of this clean up work also overlaps with cleaning
    Arduino outputs. This commit only affects makefile builds.
    
    Change-Id: Idcfce740f1ec5766599a1f0aafb5e635647e7692
    Signed-off-by: Joseph Morrow <joseph.l.morrow@intel.com>
    Signed-off-by: Doug Hudson <douglas.hudson@intel.com>
    Reviewed-on: https://gerrit.iotivity.org/gerrit/129
    Tested-by: jenkins-iotivity <jenkins-iotivity@opendaylight.org>
    Reviewed-by: Sashi Penta <sashi.kumar.penta@intel.com>
    Reviewed-by: Sudarshan Prasad <sudarshan.prasad@intel.com>

--- resource/csdk/connectivity/build/arduino/build.sh
+++ resource/csdk/connectivity/build/arduino/build.sh
@@ -1,4 +1,11 @@
 #!/bin/sh
+if echo $1 | grep -q -i "clean"
+then
+cd ../lib/libcoap-4.1.1/
+make -f makefile_arduino.mak clean
+cd ../../build/arduino
+make clean
+else
 if echo $1 | grep -q -i "make"
 then
 cd ../lib/libcoap-4.1.1/
@@ -10,4 +17,5 @@ cd ../lib/libcoap-4.1.1/
 make $1 -f makefile_arduino.mak PLATFORM=$2 ARDUINO_DIR=$4
 cd ../../build/arduino/
 make $1 PLATFORM=$2 TRANSPORT=$3 ARDUINO_DIR=$4 APP_NAME=$5
+fi
 fi
