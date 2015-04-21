7726940201c528c6e46fd1b90167abf78cdca336
js126.lee <js126.lee@samsung.com>
2015-04-21 21:26:52 +0900

    Provisioning Client sample using Provisioning API
    Client sample is using provisioningAPI to provision security resources.
    Provisioning client is able to create ACL for the device.
    
    -Patch 12 : Resolved merge conflict
    -Patch 13,14 : Removed tab
    -Patch 15 : Change define values of permission
    -Patch 16 : Modify sconscript for provisioning client sample
    
    Change-Id: I5f3b3f4bf5b1ad4c36cd6b393ba471b82aeddf35
    Signed-off-by: js126.lee <js126.lee@samsung.com>
    Reviewed-on: https://gerrit.iotivity.org/gerrit/796
    Tested-by: jenkins-iotivity <jenkins-iotivity@opendaylight.org>
    Reviewed-by: Sachin Agrawal <sachin.agrawal@intel.com>

---
+++ resource/csdk/security/provisioning/sample/provisioningclient.c
@@ -0,0 +1,339 @@
+/******************************************************************
+*
+* Copyright 2015 Samsung Electronics All Rights Reserved.
+*
+*
+*
+* Licensed under the Apache License, Version 2.0 (the "License");
+* you may not use this file except in compliance with the License.
+* You may obtain a copy of the License at
+*
+*      http://www.apache.org/licenses/LICENSE-2.0
+*
+* Unless required by applicable law or agreed to in writing, software
+* distributed under the License is distributed on an "AS IS" BASIS,
+* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+* See the License for the specific language governing permissions and
+* limitations under the License.
+*
+******************************************************************/
+
+#include <stdio.h>
+#include <string.h>
+#include <unistd.h>
+#include <getopt.h>
+#include "logger.h"
+#include "ocmalloc.h"
+#include "utlist.h"
+#include "securevirtualresourcetypes.h"
+#include "provisioningmanager.h"
+
+#define MAX_URI_LENGTH (64)
+#define MAX_PERMISSION_LENGTH (5)
+#define MAX_INPUT_ID_LENGTH (16)
+#define PREDEFINED_TIMEOUT (10)
+#define CREATE (1)
+#define READ (2)
+#define UPDATE (4)
+#define DELETE (8)
+#define NOTIFY (16)
+#define DASH '-'
+#define TAG  "provisioningclient"
+
+/**
+ * Dummy device ID
+ */
+static OicUuid_t gDeviceUUID = {"adminDeviceUUID"};
+static OicSecAcl_t        *gAcl = NULL;
+
+/**
+ * Perform cleanup for ACL list
+ *
+ * @param[in]    ACL list
+ */
+static void DeleteACLList(OicSecAcl_t *acl)
+{
+    if (acl)
+    {
+        OicSecAcl_t *aclTmp1 = NULL;
+        OicSecAcl_t *aclTmp2 = NULL;
+        LL_FOREACH_SAFE(acl, aclTmp1, aclTmp2)
+        {
+            LL_DELETE(acl, aclTmp1);
+
+            /* Clean Resources */
+            for (int i = 0; i < aclTmp1->resourcesLen; i++)
+            {
+                OCFree(aclTmp1->resources[i]);
+            }
+            OCFree(aclTmp1->resources);
+
+            /* Clean Owners */
+            OCFree(aclTmp1->owners);
+
+            /* Clean ACL node itself */
+            OCFree(aclTmp1);
+        }
+        acl = NULL;
+    }
+}
+
+/**
+ * Calculate ACL permission from string to bit
+ *
+ * @param[in] temp_psm    Input data of ACL permission string
+ * @param[in,out] pms    The pointer of ACL permission value
+ * @return  0 on success otherwise a positive error value
+ * @retval  SP_RESULT_SUCCESS    Successful
+ * @retval  SP_RESULT_INVALID_PARAM    Invaild input arguments
+ */
+static SPResult CalculateAclPermission(const char *temp_pms, uint16_t *pms)
+{
+    int i = 0;
+
+    if (NULL == temp_pms || NULL == pms)
+    {
+        return SP_RESULT_INVALID_PARAM;
+    }
+    *pms = 0;
+    while (temp_pms[i] != '\0')
+    {
+        switch (temp_pms[i])
+        {
+            case 'C':
+                {
+                    *pms += CREATE;
+                    i++;
+                    break;
+                }
+            case 'R':
+                {
+                    *pms += READ;
+                    i++;
+                    break;
+                }
+            case 'U':
+                {
+                    *pms += UPDATE;
+                    i++;
+                    break;
+                }
+            case 'D':
+                {
+                    *pms += DELETE;
+                    i++;
+                    break;
+                }
+            case 'N':
+                {
+                    *pms += NOTIFY;
+                    i++;
+                    break;
+                }
+            case '_':
+                {
+                    i++;
+                    break;
+                }
+            default:
+                {
+                    return SP_RESULT_INVALID_PARAM;
+                }
+        }
+    }
+    return SP_RESULT_SUCCESS;
+}
+
+/**
+ * Get the ACL property from user
+ *
+ * @param[in]    ACL Datastructure to save user inputs
+ * @return  0 on success otherwise a positive error value
+ * @retval  SP_RESULT_SUCCESS    Successful
+ * @retval  SP_RESULT_MEM_ALLOCATION_FAIL    Memmory allocation failure
+ */
+static SPResult InputACL(OicSecAcl_t *acl)
+{
+    char temp_id [MAX_INPUT_ID_LENGTH + 4] = {0,};
+    char temp_rsc[MAX_URI_LENGTH + 1] = {0,};
+    char temp_pms[MAX_PERMISSION_LENGTH + 1] = {0,};
+    printf("******************************************************************************\n");
+    printf("-Set ACL policy for target device\n");
+    printf("******************************************************************************\n");
+    //Set Subject.
+    printf("-URN identifying the subject\n");
+    printf("ex) 1111-1111-1111-1111 (16 Numbers except to '-')\n");
+    printf("Subject : ");
+    scanf("%19s", temp_id);
+    int j = 0;
+    for (int i = 0; temp_id[i] != '\0'; i++)
+    {
+        if (DASH != temp_id[i])
+            acl->subject.id[j++] = temp_id[i] - '0';
+    }
+    //Set Resource.
+    printf("Num. of Resource : ");
+    scanf("%zu", &acl->resourcesLen);
+    printf("-URI of resource\n");
+    printf("ex)/oic/sh/temp/0 (Max_URI_Length: 64 Byte )\n");
+    acl->resources = (char **)OCMalloc(acl->resourcesLen * sizeof(char *));
+    if (NULL == acl->resources)
+    {
+        OC_LOG(ERROR, TAG, "Error while memory allocation");
+        return SP_RESULT_MEM_ALLOCATION_FAIL;
+    }
+    for (int i = 0; i < acl->resourcesLen; i++)
+    {
+        printf("[%d]Resource : ", i + 1);
+        scanf("%64s", temp_rsc);
+        acl->resources[i] = (char *)OCMalloc((strlen(temp_rsc) + 1) * sizeof(char));
+        if (NULL == acl->resources[i])
+        {
+            OC_LOG(ERROR, TAG, "Error while memory allocation");
+            return SP_RESULT_MEM_ALLOCATION_FAIL;
+        }
+        strncpy(acl->resources[i], temp_rsc, strlen(temp_rsc));
+        acl->resources[i][strlen(temp_rsc)] = '\0';
+    }
+    // Set Permission
+    do
+    {
+        printf("-Set the permission(C,R,U,D,N)\n");
+        printf("ex) CRUDN, CRU_N,..(5 Charaters)\n");
+        printf("Permission : ");
+        scanf("%5s", temp_pms);
+    }
+    while (SP_RESULT_SUCCESS != CalculateAclPermission(temp_pms, &(acl->permission)) );
+    // Set Rowner
+    printf("Num. of Rowner : ");
+    scanf("%zu", &acl->ownersLen);
+    printf("-URN identifying the rowner\n");
+    printf("ex) 1111-1111-1111-1111 (16 Numbers except to '-')\n");
+    acl->owners = (OicUuid_t *)OCCalloc(acl->ownersLen, sizeof(OicUuid_t));
+    if (NULL == acl->owners)
+    {
+        OC_LOG(ERROR, TAG, "Error while memory allocation");
+        return SP_RESULT_MEM_ALLOCATION_FAIL;
+    }
+    for (int i = 0; i < acl->ownersLen; i++)
+    {
+        printf("[%d]Rowner : ", i + 1);
+        scanf("%19s", temp_id);
+        j = 0;
+        for (int k = 0; temp_id[k] != '\0'; k++)
+        {
+            if (DASH != temp_id[k])
+            {
+                acl->owners[i].id[j++] = temp_id[k] - '0';
+            }
+        }
+    }
+    return SP_RESULT_SUCCESS;
+}
+
+/**
+ * Provisioning client sample using ProvisioningAPI on provisioningmanager.c
+ * To change network type use command line option -w for Wifi and -e for Ethernet
+ */
+int main(int argc, char **argv)
+{
+    int c = 0;
+    int flag = 0;
+    SPResult res = SP_RESULT_SUCCESS;
+    SPConnectivityType networkType = SP_CONN_ETHERNET;
+    SPTargetDeviceInfo_t *pDeviceList = NULL;
+    gAcl = (OicSecAcl_t *)OCMalloc(sizeof(OicSecAcl_t));
+    if (NULL == gAcl)
+    {
+        OC_LOG(ERROR, TAG, "Error while memory allocation");
+        return SP_RESULT_MEM_ALLOCATION_FAIL;
+    }
+
+    while ((c = getopt(argc, argv, "ew")) != -1)
+    {
+        switch (c)
+        {
+            case 'e':
+                {
+                    printf("Network type Ethernet\n");
+                    networkType = SP_CONN_ETHERNET;
+                    flag = 1;
+                    break;
+                }
+            case 'w':
+                {
+                    printf("Network type Wifi\n");
+                    networkType = SP_CONN_WIFI;
+                    flag = 1;
+                    break;
+                }
+            default:
+                {
+                    break;
+                }
+        }
+    }
+    if (!flag)
+    {
+        printf("******************************************************************************\n");
+        printf("starting provisioning server with default network type: Ethernet \n");
+        printf("To change network type use command line option -w for Wifi and -e for Ethernet\n");
+        printf("******************************************************************************\n");
+    }
+    res = SPProvisioningDiscovery(PREDEFINED_TIMEOUT, networkType, &pDeviceList);
+    if (SP_RESULT_SUCCESS == res)
+    {
+        while (pDeviceList != NULL)
+        {
+            printf("-Provisioning device ID : ");
+            for (int i = 0; i < MAX_INPUT_ID_LENGTH; i++)
+            {
+                if (pDeviceList->deviceId.id[i] == '\0')
+                {
+                     break;
+                }
+                printf("%c", pDeviceList->deviceId.id[i]);
+            }
+            printf("\n");
+            res = SPInitProvisionContext(PREDEFINED_TIMEOUT, &gDeviceUUID, pDeviceList);
+            if (SP_RESULT_SUCCESS != res)
+            {
+                OC_LOG(ERROR, TAG, "Error while init provisioning Context");
+                goto error;
+            }
+            res = InputACL(gAcl);
+            if (SP_RESULT_SUCCESS != res)
+            {
+                OC_LOG(ERROR, TAG, "Error while user ACL input ");
+                goto error;
+            }
+            res = SPProvisionACL(PREDEFINED_TIMEOUT, pDeviceList, gAcl);
+            if (SP_RESULT_SUCCESS != res)
+            {
+                OC_LOG(ERROR, TAG, "Error while provisioning ACL");
+                goto error;
+            }
+            res = SPFinalizeProvisioning(PREDEFINED_TIMEOUT, pDeviceList);
+            if (SP_RESULT_SUCCESS == res)
+            {
+                printf("Provisioning Success~!!\n");
+            }
+            else if (SP_RESULT_SUCCESS != res)
+            {
+                OC_LOG(ERROR, TAG, "Error while Finalize Provisioning");
+                goto error;
+            }
+            pDeviceList = pDeviceList->next;
+        }
+    }
+    else
+    {
+        OC_LOG(ERROR, TAG, "Error while Provisioning Discovery");
+        goto error;
+    }
+
+error:
+    DeleteACLList(gAcl);
+    SPTerminateProvisioning();
+    return 0;
+}
