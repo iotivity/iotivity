0132ba38de77c51b3d0a5fa50d953a61533b8357
Erich Keane <erich.keane@intel.com>
2015-05-21 15:47:24 -0700

    Implementation of OICStrcat/OICStrcpy
    Repeated mistakes based on copy and cat operations in the C
    code shows that we need a unified implementation that is as safe as
    possible.  These implementations closely emulate the Microsoft _S
    versions of these functions, which seem to be safest feature wise.
    
    Replacing copy and cat operations is reserved for a different
    commit.
    
    Change-Id: Id1f6f4515a00679661cc722419fa0bab84a624c2
    Signed-off-by: Erich Keane <erich.keane@intel.com>
    Reviewed-on: https://gerrit.iotivity.org/gerrit/1115
    Tested-by: jenkins-iotivity <jenkins-iotivity@opendaylight.org>
    Reviewed-by: John Light <john.j.light@intel.com>

--- resource/csdk/connectivity/common/inc/oic_string.h
+++ resource/csdk/connectivity/common/inc/oic_string.h
@@ -20,6 +20,7 @@
 #ifndef OIC_STRING_H_
 #define OIC_STRING_H_
 
+#include <stddef.h>
 #ifdef __cplusplus
 extern "C"
 {
@@ -28,17 +29,69 @@ extern "C"
 /**
  * Duplicates the source string and returns it.
  *
- * NOTE: Caller needs to clear this memory by calling OICFree.
+ * @note Caller needs to release this memory by calling @ref OICFree.
  *
- * @param str - Original valid string which needs to be duplicated
+ * @param str Original valid string which needs to be duplicated.
  *
- * @return
- *     on success, a pointer to the duplicated string
- *     on failure, a null pointer is returned
+ * @return a pointer to the duplicated string
  */
 char *OICStrdup(const char *str);
+
+/**
+ * Copies a C string into destination buffer.  Ensures that the destination
+ * is null terminated.
+ *
+ * @param dest Destination C buffer.
+ * @param destSize The allocated size of the destination parameter.
+ * @param source Source C string.
+ *
+ * @return
+ *      returns a pointer to the passed in 'dest' parameter
+ */
+char* OICStrcpy(char* dest, size_t destSize, const char* source);
+
+/**
+ * Appends a C string into a previously allocated and initialized C string in a buffer. dest
+ * parameter is guaranteed to be null-terminated.
+ *
+ * @param dest Destination C buffer containing initial string.
+ * @param destSize The allocated size of the destination parameter.
+ * @param source Source C string.
+ *
+ * @return
+ *      returns a pointer to the passed in 'dest' parameter
+ */
+char* OICStrcat(char* dest, size_t destSize, const char* source);
+
+/**
+ * Copies a partial C string into destination buffer.
+ * Ensures that the destination is null terminated.
+ *
+ * @param dest Destination C buffer.
+ * @param destSize The allocated size of the destination parameter.
+ * @param source Source C string.
+ * @param sourceLen maximum number of characters to copy.
+ *
+ * @return
+ *      returns a pointer to the passed in 'dest' parameter
+ */
+char* OICStrcpyPartial(char* dest, size_t destSize, const char* source, size_t sourceLen);
+
+/**
+ * Appends a C string into a previously allocated and initialized C string in a buffer. dest
+ * parameter is guaranteed to be null-terminated.
+ *
+ * @param dest Destination C buffer containing initial string.
+ * @param destSize The allocated size of the destination parameter.
+ * @param source Source C string.
+ * @param sourceLen maximum number of characters to append, not including null termination
+ *
+ * @return
+ *      returns a pointer to the passed in 'dest' parameter
+ */
+char* OICStrcatPartial(char* dest, size_t destSize, const char* source, size_t sourceLen);
 
 #ifdef __cplusplus
 }
 #endif // __cplusplus
-#endif /* OIC_STRING_H_ */
+#endif // OIC_STRING_H_
--- resource/csdk/connectivity/common/src/oic_string.c
+++ resource/csdk/connectivity/common/src/oic_string.c
@@ -20,10 +20,15 @@
 #include "oic_string.h"
 
 #include <string.h>
+#include <assert.h>
+#include "logger.h"
 #include "oic_malloc.h"
 
+#define TAG "OIC_STRING"
 char *OICStrdup(const char *str)
 {
+    assert(str);
+
     // Allocate memory for original string length and 1 extra byte for '\0'
     size_t length = strlen(str);
     char *dup = (char *)OICMalloc(length + 1);
@@ -31,6 +36,65 @@ char *OICStrdup(const char *str)
     {
         memcpy(dup, str, length + 1);
     }
+    else
+    {
+        assert(NULL != dup);
+    }
+
     return dup;
 }
 
+char* OICStrcpy(char* dest, size_t destSize, const char* source)
+{
+    return OICStrcpyPartial(dest, destSize, source, destSize);
+}
+
+char* OICStrcat(char* dest, size_t destSize, const char* source)
+{
+    return OICStrcatPartial(dest, destSize, source, destSize);
+}
+
+static size_t min3(size_t a, size_t b, size_t c)
+{
+    return a < b ? (a < c ? a : c) : (b < c ? b: c);
+}
+
+char* OICStrcpyPartial(char* dest, size_t destSize, const char* source, size_t sourceLen)
+{
+    assert(dest);
+    assert(source);
+
+    if(destSize == 0 || sourceLen == 0)
+    {
+        return dest;
+    }
+
+    size_t limit = min3(destSize - 1, sourceLen, strlen(source));
+    memcpy(dest, source, limit);
+    dest[limit] = '\0';
+
+    return dest;
+}
+
+char* OICStrcatPartial(char* dest, size_t destSize, const char* source, size_t sourceLen)
+{
+    assert(dest);
+    assert(source);
+
+    if(destSize == 0 || sourceLen == 0)
+    {
+        return dest;
+    }
+
+    size_t destLen = strlen(dest);
+
+    if (destLen >= destSize)
+    {
+        return dest;
+    }
+
+    size_t limit = min3(destSize - destLen - 1, sourceLen, strlen(source));
+    memcpy(dest + destLen, source, limit);
+    dest[destLen + limit] = '\0';
+    return dest;
+}
