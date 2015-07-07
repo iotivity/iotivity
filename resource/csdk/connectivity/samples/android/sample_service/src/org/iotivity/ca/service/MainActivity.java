1eb729622d44ebb4f28f17f408f283c3fbf467d1
jihwan.seo <jihwan.seo@samsung.com>
2015-07-07 21:16:05 +0900

    fixed Android Sample App build issue
    to import Log was missed
    so it was not built
    
    Change-Id: I702b220bddd9ba0243a47eeca718326eb17fa9f6
    Signed-off-by: jihwan.seo <jihwan.seo@samsung.com>
    Reviewed-on: https://gerrit.iotivity.org/gerrit/1561
    Tested-by: jenkins-iotivity <jenkins-iotivity@opendaylight.org>
    Reviewed-by: Erich Keane <erich.keane@intel.com>

--- resource/csdk/connectivity/samples/android/sample_service/src/org/iotivity/ca/service/MainActivity.java
+++ resource/csdk/connectivity/samples/android/sample_service/src/org/iotivity/ca/service/MainActivity.java
@@ -5,6 +5,7 @@ import android.app.AlertDialog;
 import android.content.DialogInterface;
 import android.os.Bundle;
 import android.os.Handler;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
