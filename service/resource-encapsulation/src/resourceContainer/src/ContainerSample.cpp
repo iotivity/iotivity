230032b020be103fe434d1f7f460515441c33fcf
hunje.yeon <hunje.yeon@samsung.com>
2015-07-26 20:31:08 +0900

    [Resource-Manipulation] Renamed Resource-Manipulation to Resource-Encapsulation
    Synch to master branch of 5d2cc1f41da12c4f2f90750b309506e10fa45947
    
    Change-Id: I5a2bbffceffbaa29db0578aa31ebda9812ad5bc2
    Signed-off-by: hunje.yeon <hunje.yeon@samsung.com>
    Reviewed-on: https://gerrit.iotivity.org/gerrit/1913
    Tested-by: jenkins-iotivity <jenkins-iotivity@opendaylight.org>
    Reviewed-by: Uze Choi <uzchoi@samsung.com>

--- service/resource-encapsulation/src/resourceContainer/src/ContainerSample.cpp
+++ service/resource-encapsulation/src/resourceContainer/src/ContainerSample.cpp
@@ -120,7 +120,7 @@ int main()
     bundleParams["libraryPath"] = ".";
     bundleParams["activator"] = "org.iotivity.bundle.hue.HueBundleActivator";
     container->addBundle("oic.bundle.hueJavaSample", "/hueJava",
-                         "../../../../../../../../service/resource-manipulation/src/resourceContainer/examples/HueJavaSampleBundle/hue/target/hue-0.1-jar-with-dependencies.jar",
+                         "../../../../../../../../service/resource-encapsulation/src/resourceContainer/examples/HueJavaSampleBundle/hue/target/hue-0.1-jar-with-dependencies.jar",
                          bundleParams);
 
     bundles = container->listBundles();
