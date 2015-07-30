e91463600876309d4186f2152f37e20ee791185d
Mandeep Shetty <mandeep.shetty@intel.com>
2015-07-30 15:53:47 -0700

    Fixed arduino server sending notifications after deregister.
    This fixes IOT-627.
    Arduino server continuted to send observe notifications even after
    client deregisters. This happened because a bool variable tracking if
    light was under observation was not set to false.
    
    Change-Id: Ic1f00915632251a9be8772ca3cc4b71373dc3b18
    Signed-off-by: Mandeep Shetty <mandeep.shetty@intel.com>
    Reviewed-on: https://gerrit.iotivity.org/gerrit/2012
    Tested-by: jenkins-iotivity <jenkins-iotivity@opendaylight.org>
    Reviewed-by: Erich Keane <erich.keane@intel.com>

--- resource/csdk/stack/samples/arduino/SimpleClientServer/ocserver/ocserver.cpp
+++ resource/csdk/stack/samples/arduino/SimpleClientServer/ocserver/ocserver.cpp
@@ -209,6 +209,7 @@ OCEntityHandlerResult OCEntityHandlerCb(OCEntityHandlerFlag flag, OCEntityHandle
         else if (OC_OBSERVE_DEREGISTER == entityHandlerRequest->obsInfo.action)
         {
             OC_LOG (INFO, TAG, PCF("Received OC_OBSERVE_DEREGISTER from client"));
+            gLightUnderObservation = 0;
         }
     }
     OCRepPayloadDestroy(payload);
