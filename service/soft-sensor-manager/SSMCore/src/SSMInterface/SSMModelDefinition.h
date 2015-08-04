eddf7d5858ffd0403a6e2b5f8ab03284526c631f
Minji Park <minjii.park@samsung.com>
2015-08-04 10:18:59 +0900

    initialize uninitialized variable of soft sensor manager
    initialize uninitialized variable of soft sensor manager
    to prevent potential problem
    
    Change-Id: Ib929923710162afc96b370f67332b18e19adbd66
    Signed-off-by: Minji Park <minjii.park@samsung.com>
    Reviewed-on: https://gerrit.iotivity.org/gerrit/2093
    Tested-by: jenkins-iotivity <jenkins-iotivity@opendaylight.org>
    Reviewed-by: Madan Lanka <lanka.madan@samsung.com>

--- service/soft-sensor-manager/SSMCore/src/SSMInterface/SSMModelDefinition.h
+++ service/soft-sensor-manager/SSMCore/src/SSMInterface/SSMModelDefinition.h
@@ -53,11 +53,13 @@ class ISSMResource
         ISSMResource()
         {
             location = SENSOR_LOCATION_LOCAL;
+            connectivityType = 0;
         }
         ISSMResource(const std::string &n, const std::string &t) :
             name(n), type(t)
         {
             location = SENSOR_LOCATION_LOCAL;
+            connectivityType = 0;
         }
         SENSOR_LOCATION location;
         std::string name;
