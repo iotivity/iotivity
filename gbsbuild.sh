01431b8ba3781808b806d0a85d5d39b18c2f3a86
js126.lee <js126.lee@samsung.com>
2015-09-23 23:06:02 +0900

    Fixed Tizen build error with SECURED=1
    [Patch-1] Initial
    [Patch-2,3] Fixed build error in case of CA partially.
    Note : The purpose of this patch is to fix build error 
    when building full tizen with security. 
    The change required in jenkins configuration is out of this patch.
    
    Change-Id: I2ec95c13d6c7d7978662e7ef69f7a02cd8188df0
    Signed-off-by: js126.lee <js126.lee@samsung.com>
    Reviewed-on: https://gerrit.iotivity.org/gerrit/2997
    Reviewed-by: Madan Lanka <lanka.madan@samsung.com>
    Tested-by: jenkins-iotivity <jenkins-iotivity@opendaylight.org>
    Reviewed-by: Sachin Agrawal <sachin.agrawal@intel.com>
    (cherry picked from commit 8a42b6a387e634c1138a82ada0df1fc43d93ad36)
    Reviewed-on: https://gerrit.iotivity.org/gerrit/3171

--- gbsbuild.sh
+++ gbsbuild.sh
@@ -23,6 +23,7 @@ cp -R ./examples $sourcedir/tmp
 cp -R ./extlibs/tinycbor $sourcedir/tmp/extlibs
 cp -R ./extlibs/cjson $sourcedir/tmp/extlibs
 cp -R ./extlibs/tinydtls $sourcedir/tmp/extlibs
+cp -R ./extlibs/sqlite3 $sourcedir/tmp/extlibs
 cp -R ./extlibs/timer $sourcedir/tmp/extlibs
 cp -R ./extlibs/rapidxml $sourcedir/tmp/extlibs
 cp -R ./resource $sourcedir/tmp
