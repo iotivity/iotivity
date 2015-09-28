96c969fa1d56f1a21f3c0a580cca92dfa671695e
Hemant Mahsky <h.mahsky@samsung.com>
2015-09-28 22:24:14 +0530

    Added Multi PHY EasySetup Service
    Change-Id: Ie8504d25c0f8c002737b8a82820e0af52944ca83
    Signed-off-by: Hemant Mahsky <h.mahsky@samsung.com>
    
    Updated SConscript to build EasySetup
    
    Change-Id: Ie8504d25c0f8c002737b8a82820e0af52944ca83
    Signed-off-by: Hemant Mahsky <h.mahsky@samsung.com>
    Reviewed-on: https://gerrit.iotivity.org/gerrit/3229
    Tested-by: jenkins-iotivity <jenkins-iotivity@opendaylight.org>
    Reviewed-by: Madan Lanka <lanka.madan@samsung.com>
    (cherry picked from commit 95818b037e2b8e3c2a738f47a107fe4088ba40b1)
    Reviewed-on: https://gerrit.iotivity.org/gerrit/3237

---
+++ service/easy-setup/sdk/mediator/android/EasySetupCore/src/main/java/org/iotivity/service/easysetup/core/EasySetupService.java
@@ -0,0 +1,186 @@
+/**
+ * ***************************************************************
+ * <p/>
+ * Copyright 2015 Samsung Electronics All Rights Reserved.
+ * <p/>
+ * <p/>
+ * <p/>
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * <p/>
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * <p/>
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * <p/>
+ * ****************************************************************
+ */
+
+package org.iotivity.service.easysetup.core;
+
+import android.content.Context;
+import android.util.Log;
+
+import org.iotivity.service.easysetup.impl.EnrolleeDeviceFactory;
+import org.iotivity.service.easysetup.mediator.EasySetupManager;
+import org.iotivity.service.easysetup.mediator.ProvisionEnrollee;
+
+import java.io.IOException;
+import java.util.ArrayList;
+
+/**
+ * This is facade class, a single point of contact for Application.
+ * It contains set of APIs to do easy setup of the enrolling device.
+ * ON-BOARDING - This is a step to establish connectivity between the device & Mediator device.
+ * PROVISION   - This is a step where the netowork's detail & credentials are given to the
+ * enrolling device.
+ */
+public class EasySetupService {
+
+    public static final String TAG = EasySetupService.class.getName();
+
+    private static EasySetupService sInstance;
+
+    private final EasySetupStatus mCallback;
+
+    private ArrayList<EnrolleeDevice> mEnrolleeDeviceList;
+
+    private final ProvisioningCallback mProvisioningCallback;
+
+    private static Context mContext;
+
+    public EnrolleeDeviceFactory mDeviceFactory;
+
+    ProvisionEnrollee mProvisionEnrolleeInstance;
+
+    private EasySetupService(EasySetupStatus callback) {
+        mCallback = callback;
+        mProvisioningCallback = new ProvisioningCallbackImpl(mCallback);
+        mEnrolleeDeviceList = new ArrayList<EnrolleeDevice>();
+        mContext = null;
+        mDeviceFactory = null;
+    }
+
+    /**
+     * Gives a singleton instance of Easy setup service and initialize the service
+     *
+     * @param callback Application needs to provide this callback to receive the status of easy
+     *                 setup process.
+     */
+
+    public synchronized static EasySetupService getInstance(Context context, EasySetupStatus
+            callback) {
+        if (sInstance == null) {
+            sInstance = new EasySetupService(callback);
+            mContext = context;
+        }
+        return sInstance;
+    }
+
+    /**
+     * Reset the Easy setup Service
+     */
+
+    public void finish() {
+
+        //Native Api call to reset OIC stack
+        if (mProvisionEnrolleeInstance != null) {
+            mProvisionEnrolleeInstance.stopEnrolleeProvisioning(0);
+        }
+    }
+
+    /**
+     * Starts Easy setup process for the enrolling device.
+     *
+     * @param enrolledevice Device to be enrolled in network
+     * @throws IOException Throws exception in case of any connection error.
+     */
+
+    public synchronized void startSetup(final EnrolleeDevice enrolledevice) throws IOException {
+
+        mEnrolleeDeviceList.add(enrolledevice);
+
+
+        // Starts the provisioning directly if the device is already on boarded on the network.
+        if (enrolledevice.onBoarded()) {
+            enrolledevice.startProvisioning(mProvisioningCallback);
+            return;
+        }
+
+        enrolledevice.startOnBoarding(new OnBoardingCallback() {
+
+            @Override
+            public void onFinished(OnBoardingConnection connection) {
+                if (connection.isConnected()) {
+                    Log.i(TAG, "On boarding is successful ");
+                    // Start provisioning here
+                    enrolledevice.setConnection(connection);
+                    enrolledevice.startProvisioning(mProvisioningCallback);
+                } else {
+                    enrolledevice.mState = EnrolleeState.DEVICE_PROVISIONING_FAILED_STATE;
+                    mProvisioningCallback.onFinished(enrolledevice);
+                }
+
+            }
+
+        });
+
+    }
+
+    /**
+     * Stops on-going Easy setup process for enrolling device.
+     *
+     * @param enrolleedevice Device to be enrolled in network
+     */
+    public synchronized void stopSetup(EnrolleeDevice enrolleedevice) {
+        if (enrolleedevice.mState == EnrolleeState.DEVICE_ON_BOARDING_STATE) {
+            if (mEnrolleeDeviceList.contains(enrolleedevice)) {
+                Log.i(TAG, "stopOnBoardingProcess for enrolleedevice");
+                enrolleedevice.stopOnBoardingProcess();
+                mEnrolleeDeviceList.remove(enrolleedevice);
+            }
+        } else if (enrolleedevice.mState == EnrolleeState.DEVICE_PROVISIONING_STATE) {
+            if (mEnrolleeDeviceList.contains(enrolleedevice)) {
+                Log.i(TAG, "stopOnBoardingProcess for enrolleedevice");
+                enrolleedevice.stopOnBoardingProcess();
+
+                Log.i(TAG, "stopEnrolleeProvisioning for enrolleedevice");
+                //Native Api call to stop on-going enrolling process for the enrolling device
+                EasySetupManager.getInstance().stopEnrolleeProvisioning(enrolleedevice
+                        .mOnBoardingConfig.getConnType().getValue());
+                mEnrolleeDeviceList.remove(enrolleedevice);
+            }
+        }
+    }
+
+    public synchronized void getEnrolleeDevice(OnBoardingConfig connType) {
+        mDeviceFactory = EnrolleeDeviceFactory.newInstance(mContext);
+    }
+
+    class ProvisioningCallbackImpl extends ProvisioningCallback {
+
+        private final EasySetupStatus mCallback;
+
+        ProvisioningCallbackImpl(EasySetupStatus callback) {
+            mCallback = callback;
+        }
+
+        @Override
+        public void onFinished(EnrolleeDevice enrolledevice) {
+            synchronized (EasySetupService.this) {
+                if (mEnrolleeDeviceList.contains(enrolledevice)) {
+                    Log.i(TAG, "onFinished() is received " + enrolledevice.isSetupSuccessful());
+                    mCallback.onFinished(enrolledevice);
+                    mEnrolleeDeviceList.remove(enrolledevice);
+                }
+            }
+        }
+
+    }
+
+
+}
---
+++ service/easy-setup/sdk/mediator/src/prov_adapter.cpp
@@ -0,0 +1,96 @@
+//******************************************************************
+//
+// Copyright 2015 Samsung Electronics All Rights Reserved.
+//
+//-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+//
+//-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <signal.h>
+#include <unistd.h>
+#include <stdint.h>
+#include <sstream>
+
+#include "prov_adapter.h"
+
+//Use ipv4addr for both InitDiscovery and InitDeviceDiscovery
+char ipv4addr[IPV4_ADDR_SIZE] = {0};
+
+volatile static OCProvisioningStatusCB cbData = NULL;
+
+OCStackResult InitProvProcess() {
+
+
+    OCStackResult result = OC_STACK_ERROR;
+
+    if (InitProvisioningHandler() == OC_STACK_OK) {
+        result = OC_STACK_OK;
+        OIC_LOG(DEBUG, TAG, "InitProvisioningHandler returned Success");
+    } else {
+        result = OC_STACK_ERROR;
+        OIC_LOG_V(ERROR, TAG, "InitProvisioningHandler returned error = %s",
+                  result);
+    }
+
+    return result;
+}
+
+OCStackResult ResetProvProcess() {
+    return TerminateProvisioningHandler();
+}
+
+OCStackResult RegisterCallback(
+        OCProvisioningStatusCB provisioningStatusCallback) {
+    OCStackResult result = OC_STACK_OK;
+
+    if (provisioningStatusCallback != NULL) {
+        cbData = provisioningStatusCallback;
+    }
+    else {
+        result = OC_STACK_ERROR;
+        OIC_LOG(ERROR, TAG, "provisioningStatusCallback is NULL");
+    }
+
+    return result;
+}
+
+void UnRegisterCallback() {
+    if (cbData) {
+        cbData = NULL;
+    }
+}
+
+OCStackResult StartProvisioning(const EnrolleeNWProvInfo_t *netInfo) {
+
+    char findQuery[64] = {0};
+
+    if (netInfo->connType == CT_IP_USE_V4) {
+        snprintf(findQuery, sizeof(findQuery), UNICAST_PROVISIONING_QUERY,
+                 netInfo->netAddressInfo.WIFI.ipAddress, IP_PORT);
+    }
+
+    return StartProvisioningProcess(netInfo, cbData, findQuery);
+}
+
+OCStackResult StopProvisioning(OCConnectivityType connectivityType) {
+    OCStackResult result = OC_STACK_OK;
+
+    StopProvisioningProcess();
+
+    return result;
+}
+
