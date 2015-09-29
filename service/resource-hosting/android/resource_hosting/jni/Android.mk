1231ae73db59795d2571d9f67e7c6a72e0d443a9
Jay Sharma <jay.sharma@samsung.com>
2015-09-29 12:33:35 +0530

    [resource-hosting] Updated the Java SDK as per new folder structure.
    - Updated SDK.
    - Updated Android Sample Applications.
    
    Change-Id: Ifea8bb81528dceea0879729b06bf0f90fe9beb05
    Signed-off-by: Jay Sharma <jay.sharma@samsung.com>
    Reviewed-on: https://gerrit.iotivity.org/gerrit/3251
    Reviewed-by: Abhishek Pandey <abhi.siso@samsung.com>
    Tested-by: jenkins-iotivity <jenkins-iotivity@opendaylight.org>
    Reviewed-by: Madan Lanka <lanka.madan@samsung.com>

--- service/resource-hosting/android/resource_hosting/jni/Android.mk
+++ service/resource-hosting/android/resource_hosting/jni/Android.mk
@@ -1,67 +1,67 @@
 LOCAL_PATH := $(call my-dir)
 
 include $(CLEAR_VARS)
-OIC_LIB_PATH := ../../../../../../android/android_api/base/libs/$(TARGET_ARCH_ABI)
+OIC_LIB_PATH := ../../../../../android/android_api/base/libs/$(TARGET_ARCH_ABI)
 LOCAL_MODULE := ca_interface
 LOCAL_SRC_FILES := $(OIC_LIB_PATH)/libca-interface.so
 include $(PREBUILT_SHARED_LIBRARY)
 
 include $(CLEAR_VARS)
-OIC_LIB_PATH := ../../../../../../out/android/$(TARGET_ARCH_ABI)/release
+OIC_LIB_PATH := ../../../../../out/android/$(TARGET_ARCH_ABI)/release
 LOCAL_MODULE := ca
 LOCAL_SRC_FILES := $(OIC_LIB_PATH)/libconnectivity_abstraction.so
 include $(PREBUILT_SHARED_LIBRARY)
 
 include $(CLEAR_VARS)
-OIC_LIB_PATH := ../../../../../../out/android/$(TARGET_ARCH_ABI)/release
+OIC_LIB_PATH := ../../../../../out/android/$(TARGET_ARCH_ABI)/release
 LOCAL_MODULE := oc_logger_core
 LOCAL_SRC_FILES := $(OIC_LIB_PATH)/liboc_logger_core.so
 include $(PREBUILT_SHARED_LIBRARY)
 
 include $(CLEAR_VARS)
-OIC_LIB_PATH := ../../../../../../out/android/$(TARGET_ARCH_ABI)/release
+OIC_LIB_PATH := ../../../../../out/android/$(TARGET_ARCH_ABI)/release
 LOCAL_MODULE := oc_logger
 LOCAL_SRC_FILES := $(OIC_LIB_PATH)/liboc_logger.so
 include $(PREBUILT_SHARED_LIBRARY)
 
 include $(CLEAR_VARS)
-OIC_LIB_PATH := ../../../../../../out/android/$(TARGET_ARCH_ABI)/release
+OIC_LIB_PATH := ../../../../../out/android/$(TARGET_ARCH_ABI)/release
 LOCAL_MODULE := octbstack
 LOCAL_SRC_FILES := $(OIC_LIB_PATH)/liboctbstack.so
 include $(PREBUILT_SHARED_LIBRARY)
 
 include $(CLEAR_VARS)
-OIC_LIB_PATH := ../../../../../../out/android/$(TARGET_ARCH_ABI)/release
+OIC_LIB_PATH := ../../../../../out/android/$(TARGET_ARCH_ABI)/release
 LOCAL_MODULE := oc
 LOCAL_SRC_FILES := $(OIC_LIB_PATH)/liboc.so
 include $(PREBUILT_SHARED_LIBRARY)
 
 include $(CLEAR_VARS)
-OIC_LIB_PATH := ../../../../../../android/android_api/base/libs/$(TARGET_ARCH_ABI)
+OIC_LIB_PATH := ../../../../../android/android_api/base/libs/$(TARGET_ARCH_ABI)
 LOCAL_MODULE := ocstack-jni
 LOCAL_SRC_FILES := $(OIC_LIB_PATH)/libocstack-jni.so
 include $(PREBUILT_SHARED_LIBRARY)
 
 include $(CLEAR_VARS)
-OIC_LIB_PATH := ../../../../../../out/android/$(TARGET_ARCH_ABI)/release
+OIC_LIB_PATH := ../../../../../out/android/$(TARGET_ARCH_ABI)/release
 LOCAL_MODULE := rcsCommon
 LOCAL_SRC_FILES := $(OIC_LIB_PATH)/librcs_common.so
 include $(PREBUILT_SHARED_LIBRARY)
 
 include $(CLEAR_VARS)
-OIC_LIB_PATH := ../../../../../../out/android/$(TARGET_ARCH_ABI)/release
+OIC_LIB_PATH := ../../../../../out/android/$(TARGET_ARCH_ABI)/release
 LOCAL_MODULE := rcsClient
 LOCAL_SRC_FILES := $(OIC_LIB_PATH)/librcs_client.so
 include $(PREBUILT_SHARED_LIBRARY)
 
 include $(CLEAR_VARS)
-OIC_LIB_PATH := ../../../../../../out/android/$(TARGET_ARCH_ABI)/release
+OIC_LIB_PATH := ../../../../../out/android/$(TARGET_ARCH_ABI)/release
 LOCAL_MODULE := rcsServer
 LOCAL_SRC_FILES := $(OIC_LIB_PATH)/librcs_server.so
 include $(PREBUILT_SHARED_LIBRARY)
 
 include $(CLEAR_VARS)
-OIC_LIB_PATH := ../../../../../../out/android/$(TARGET_ARCH_ABI)/release
+OIC_LIB_PATH := ../../../../../out/android/$(TARGET_ARCH_ABI)/release
 LOCAL_MODULE := resourceHosting
 LOCAL_SRC_FILES := $(OIC_LIB_PATH)/libresource_hosting.so
 include $(PREBUILT_SHARED_LIBRARY)
@@ -83,7 +83,7 @@ LOCAL_STATIC_LIBRARIES += rcsServer
 LOCAL_STATIC_LIBRARIES += resourceHosting
 
 
-OIC_SRC_DIR := ../../../../..
+OIC_SRC_DIR := ../../../..
 
 LOCAL_C_INCLUDES := $(OIC_SRC_DIR)/resource/csdk/stack/include \
                     $(OIC_SRC_DIR)/resource/csdk/logger/include \
