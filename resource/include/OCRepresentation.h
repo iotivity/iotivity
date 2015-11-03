6f02493b568893feb27ae3345019d24ca43a1131
Erich Keane <erich.keane@intel.com>
2015-11-03 09:53:42 -0800

    Changed operator T enable_if to be more closely linked to the variant
    The link between the variant and the enable_if needs to be strong!
    Previously, there was no direct link, just one by 'coincidence'.  This
    patch introduces a pair of type traits that bind the enable_if directly
    to the variant.  Note that the type traits also work with ANY variadic
    type!
    
    Change-Id: Ie5f8fbc92b4f6800625c58fd73c08bedf147f122
    Signed-off-by: Erich Keane <erich.keane@intel.com>
    Reviewed-on: https://gerrit.iotivity.org/gerrit/4021
    Tested-by: jenkins-iotivity <jenkins-iotivity@opendaylight.org>
    Reviewed-by: Ossama Othman <ossama.othman@intel.com>
    Reviewed-by: Habib Virji <habib.virji@samsung.com>

--- resource/include/OCRepresentation.h
+++ resource/include/OCRepresentation.h
@@ -266,27 +266,10 @@ namespace OC
                     // ambigious conversions in the case where conversions can include a number of
                     // types, such as the string constructor.
                     template<typename T, typename std::enable_if<
-                     std::is_same<T, int>::value ||
-                     std::is_same<T, double>::value ||
-                     std::is_same<T, bool>::value ||
-                     std::is_same<T, std::string>::value ||
-                     std::is_same<T, OCRepresentation>::value ||
-                     std::is_same<T, std::vector<int>>::value ||
-                     std::is_same<T, std::vector<std::vector<int>>>::value ||
-                     std::is_same<T, std::vector<std::vector<std::vector<int>>>>::value ||
-                     std::is_same<T, std::vector<double>>::value ||
-                     std::is_same<T, std::vector<std::vector<double>>>::value ||
-                     std::is_same<T, std::vector<std::vector<std::vector<double>>>>::value ||
-                     std::is_same<T, std::vector<bool>>::value ||
-                     std::is_same<T, std::vector<std::vector<bool>>>::value ||
-                     std::is_same<T, std::vector<std::vector<std::vector<bool>>>>::value ||
-                     std::is_same<T, std::vector<std::string>>::value ||
-                     std::is_same<T, std::vector<std::vector<std::string>>>::value ||
-                     std::is_same<T, std::vector<std::vector<std::vector<std::string>>>>::value ||
-                     std::is_same<T, std::vector<OCRepresentation>>::value ||
-                     std::is_same<T, std::vector<std::vector<OCRepresentation>>>::value ||
-                     std::is_same<T, std::vector<std::vector<std::vector<OCRepresentation>>>>::value
-                     , int>::type = 0// enable_if
+                        is_component<T,
+                            remove_first<AttributeValue>::type
+                            >::value
+                        , int>::type = 0
                     >
                     operator T() const
                     {
