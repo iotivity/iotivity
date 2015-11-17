6e28f895b0c85ded2655538d6641688a3c6ef82d
Sanjeev BA <iamsanjeev@gmail.com>
2015-11-18 00:12:43 +0900

    Fix scenario #1.
    Change-Id: I3607022d21123ac21e07d94b365a2f03998cb038
    Signed-off-by: Sanjeev BA <iamsanjeev@gmail.com>
    Reviewed-on: https://gerrit.iotivity.org/gerrit/4243
    Tested-by: jenkins-iotivity <jenkins-iotivity@opendaylight.org>
    Reviewed-by: yw sung <yw.sung@samsung.com>

--- wsi/public/controllers/scene1controller.js
+++ wsi/public/controllers/scene1controller.js
@@ -8,41 +8,82 @@
             }
         ]
         
+        var findresource = {
+            "cid": "org.iotivity.findresource",                    
+            "endpointtype": "IOTIVITY",
+            "operation": "GET",
+            "resourceType" : "all"
+        };
+                
+        var getresource = {
+            "cid": "org.iotivity.getresource",
+            "endpoint": "oic://{{address}}:{{port}}/{{uri}}",
+            "endpointtype": "IOTIVITY",
+            "operation": "GET",
+            "resourceID" : "",
+            "params": 
+            {
+                "address": "server ip address",
+                "port": "server port",
+                "uri": "server's uri"
+            }
+        };
+        
+        var putresource = {
+            "cid": "org.iotivity.putresource",
+            "endpoint": "oic://{{address}}:{{port}}/{{uri}}",
+            "endpointtype": "IOTIVITY",
+            "operation": "POST",
+            "resourceID" : "",
+            "params": {
+                  "address": "server ip address",
+                  "port": "server port",
+                  "uri": "server's uri"
+             },
+            "payload": {
+                 "property" : "value"
+             },
+            "tags": [
+              "put reosurce properties and value"
+            ]
+        };
+
         var posttweet = {
-                    "cid": "com.twitter.post",
-                    "isauthrequired": "true",
-                    "description": "Post Message to Twiiter.",
-                    "endpoint": "statuses/update",
-                    "endpointtype": "twitter",
-                    "operation": "POSTTWEET",
-                    "params": {
-                        "text": "Message to post",
-                        "screen_name" : "oicdemo"
-                    },
-                    "tags": [
-                        "share",
-                        "post"
-                    ]
-                };
+            "cid": "com.twitter.post",
+            "isauthrequired": "true",
+            "description": "Post Message to Twiiter.",
+            "endpoint": "statuses/update",
+            "endpointtype": "twitter",
+            "operation": "POSTTWEET",
+            "params": {
+                "text": "Message to post",
+                "screen_name" : "oicdemo"
+            },
+            "tags": [
+                "share",
+                "post"
+            ]
+        };
                 
         var readtweet = {
-                    "cid": "com.twitter.read",
-                    "isauthrequired": "true",
-                    "description": "Get Latest Message from Twiiter.",
-                    "endpoint": "statuses/user_timeline",
-                    "endpointtype": "twitter",
-                    "operation": "READTWEET",
-                    "params": {
-                        "text": "Get message from twitter",
-                        "screen_name" : "oicdemo"
-                    },
-                    "tags": [
-                        "share",
-                        "post"
-                    ]
-                };
+            "cid": "com.twitter.read",
+            "isauthrequired": "true",
+            "description": "Get Latest Message from Twiiter.",
+            "endpoint": "statuses/user_timeline",
+            "endpointtype": "twitter",
+            "operation": "READTWEET",
+            "params": {
+                "text": "Get message from twitter",
+                "screen_name" : "oicdemo"
+            },
+            "tags": [
+                "share",
+                "post"
+            ]
+        };
                 
-        document.getElementById('tweetcap').value = JSON.stringify(posttweet);
+        document.getElementById('tweetcap').value = JSON.stringify(readtweet);
+        document.getElementById('iotivitycap').value = JSON.stringify(findresource);
 
         $scope.readTweet = function(){
             document.getElementById('tweetcap').value = JSON.stringify(readtweet);
@@ -65,6 +106,35 @@
                 $scope.scene1updates.push({title: 'Failure', content: data});
             });
         };
+        
+        $scope.findIoTivityDevices = function() {
+            document.getElementById('iotivitycap').value = JSON.stringify(findresource);
+        };
+        
+        $scope.getLightColor = function() {
+            document.getElementById('iotivitycap').value = JSON.stringify(getresource);
+        };
+        
+        $scope.setLightColor = function() {
+            document.getElementById('iotivitycap').value = JSON.stringify(putresource);
+        };
+
+        $scope.executeIoTivityCap = function() {
+            var uri = location.origin + "/wsi/cap/post/org.iotivity";
+            console.log("Making a POST HTTP Request " + uri);
+            reqbody = JSON.parse(document.getElementById('iotivitycap').value);
+            
+            var res = $http.post(uri, reqbody);
+            res.success(function(data, status, headers, config) {
+                console.log("Success Response = " + data );
+                $scope.scene1updates.push({title: 'Success', content: data});
+            });
+            res.error(function(data, status, headers, config) {
+                console.log("Failed Response = " + data );
+                $scope.scene1updates.push({title: 'Failure', content: data});
+            });
+        };
+        
     };
 
     scene1controller.$inject = ['$scope', '$http'];
