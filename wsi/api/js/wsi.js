7c06a6d4a4019fd5032c9ac128b0f69397f3c140
Sanjeev BA <as2902.b@samsung.com>
2015-12-21 11:43:53 +0900

    Add API for Examples.
    Change-Id: Iafe46a3c3068727e6c27429f86f43feb7e32a971
    Signed-off-by: Sanjeev BA <as2902.b@samsung.com>
    Reviewed-on: https://gerrit.iotivity.org/gerrit/4679
    Tested-by: jenkins-iotivity <jenkins-iotivity@opendaylight.org>
    Reviewed-by: yw sung <yw.sung@samsung.com>

---
+++ wsi/api/js/wsi.js
@@ -0,0 +1,189 @@
+/*
+*  WSI SDK for Javascript Apps.
+*
+*  @author Samsung OSG (as2902.b@samsung.com)
+*/
+
+
+/**
+ * expose our sdk
+ */
+(function(root) {
+  root.WSISDK = root.WSISDK || {};
+  root.WSISDK.VERSION = "1.0.0";
+}(this));
+
+/**
+ * main sdk
+ */
+(function(root) {
+
+    root.WSISDK = root.WSISDK || {};
+
+    /**
+    * Contains all WSISDK API classes and functions.
+    * @name WSISDK
+    * @namespace
+    *
+    * Contains all WSISDK API classes and functions.
+    */
+    var WSISDK = root.WSISDK;
+
+    // If jQuery has been included, grab a reference to it.
+    if (typeof(root.$) !== "undefined") {
+        WSISDK.$ = root.$;
+    }
+
+    /**
+     * Call this method first to set your authentication key.
+     * @param {String} API Token
+     */
+    WSISDK.Initialize = function(config) {
+        WSISDK._initialize(config);
+    };
+    /**
+     * This method is for WSISDK's own private use.
+     * @param {String} API Token
+     */
+    WSISDK._initialize = function(config) {
+        WSISDK.config = config;
+        
+    };
+
+    /**
+     * Get All Services
+     * @param  {function} successCallback
+     */
+    WSISDK.getServices = function(successCallback) {
+        var url = WSISDK.config.serverURL + "/wsi/services";
+        var xhr = new XMLHttpRequest();;
+        xhr.open('GET', url, true);
+        xhr.onload = function() {
+            if (xhr.status >= 200 && xhr.status < 300) {
+                successCallback(xhr.response);
+            } else {
+                console.log('Error !');
+            }
+        };
+    };
+
+    /**
+     * Get a service
+     * @param  {function} id
+     * @param  {function} successCallback
+     */
+    WSISDK.deleteService = function(id, successCallback) {
+        var url = WSISDK.config.serverURL + "/wsi/service/" + id;
+        var xhr = new XMLHttpRequest();;
+        xhr.open('GET', url, true);
+        xhr.onload = function() {
+            if (xhr.status >= 200 && xhr.status < 300) {
+                successCallback(xhr.response);
+            } else {
+                console.log('Error !');
+            }
+        };
+        xhr.send();
+    };
+
+    /**
+     * Add a new service
+     * @param  {function} body
+     * @param  {function} successCallback
+     */
+    WSISDK.updateService = function(body, successCallback) {
+        var url = WSISDK.config.serverURL + "/wsi/service";
+        var xhr = new XMLHttpRequest();;
+        xhr.open('POST', url, true);
+        xhr.setRequestHeader("Content-Type", "application/json;charset=UTF-8");
+        xhr.onload = function() {
+            if (xhr.status >= 200 && xhr.status < 300) {
+                successCallback(xhr.response);
+            } else {
+                console.log('Error !');
+            }
+        };
+        xhr.send(JSON.stringify(body));
+    };
+
+    /**
+     * Delete a service
+     * @param  {function} id
+     * @param  {function} successCallback
+     */
+    WSISDK.deleteService = function(id, successCallback) {
+        var url = WSISDK.config.serverURL + "/wsi/service/" + id;
+        var xhr = new XMLHttpRequest();;
+        xhr.open('DELETE', url, true);
+        xhr.onload = function() {
+            if (xhr.status >= 200 && xhr.status < 300) {
+                successCallback(xhr.response);
+            } else {
+                console.log('Error !');
+            }
+        };
+        xhr.send();
+    };
+
+    /**
+     * Update a service
+     * @param  {function} body
+     * @param  {function} successCallback
+     */
+    WSISDK.updateService = function(body, successCallback) {
+        var url = WSISDK.config.serverURL + "/wsi/service";
+        var xhr = new XMLHttpRequest();;
+        xhr.open('PUT', url, true);
+        xhr.setRequestHeader("Content-Type", "application/json;charset=UTF-8");
+        xhr.onload = function() {
+            if (xhr.status >= 200 && xhr.status < 300) {
+                successCallback(xhr.response);
+            } else {
+                console.log('Error !');
+            }
+        };
+        xhr.send(JSON.stringify(body));
+    };
+
+    /**
+     * Provide A&A info to access a capability
+     * @param  {function} body
+     * @param  {function} successCallback
+     */
+    WSISDK.authCapability = function(body, successCallback) {
+        var url = WSISDK.config.serverURL + "/wsi/cap/auth";
+        var xhr = new XMLHttpRequest();;
+        xhr.open('POST', url, true);
+        xhr.setRequestHeader("Content-Type", "application/json;charset=UTF-8");
+        xhr.onload = function() {
+            if (xhr.status >= 200 && xhr.status < 300) {
+                successCallback(xhr.response);
+            } else {
+                console.log('Error !');
+            }
+        };
+        xhr.send(JSON.stringify(body));
+    };
+
+    /**
+     * Access a service capability
+     * @param  {function} body
+     * @param  {function} successCallback
+     */
+    WSISDK.exec = function(id, body, successCallback) {
+        var url = WSISDK.config.serverURL + "/wsi/cap/post/" + id;
+        var xhr = new XMLHttpRequest();;
+        xhr.open('POST', url, true);
+        xhr.setRequestHeader("Content-Type", "application/json;charset=UTF-8");
+        xhr.onload = function() {
+            if (xhr.status >= 200 && xhr.status < 300) {
+                successCallback(xhr.response);
+            } else {
+                console.log('Error !');
+            }
+        };
+        xhr.send(JSON.stringify(body));
+    };
+
+
+}(this));
