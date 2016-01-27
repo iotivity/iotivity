a79397b5d7b9a9e6c9335837595f0e1623e0c938
Rahul Rahul <rahul.rahul@intel.com>
2016-01-27 10:42:13 -0800

    Reverting back to Android-21 as the move to Android-23 causes Android to crash on M devices. The current build scripts will need to be updated to generate shared libs with soname. Till the scripts are updated, Android-21 will be used as it works even on M devices.
    Change-Id: I33e0ccc19fb406b3508d888f7b04617c7d6e0868
    Signed-off-by: Rahul Rahul <rahul.rahul@intel.com>
    Reviewed-on: https://gerrit.iotivity.org/gerrit/4873
    Tested-by: jenkins-iotivity <jenkins-iotivity@opendaylight.org>
    Reviewed-by: Jon A. Cruz <jonc@osg.samsung.com>

--- resource/csdk/connectivity/lib/android/jni/Application.mk
+++ resource/csdk/connectivity/lib/android/jni/Application.mk
@@ -22,7 +22,7 @@
 APP_PROJECT_PATH = ./
 
 APP_STL = gnustl_shared
-APP_PLATFORM = android-23
+APP_PLATFORM = android-21
 APP_CPPFLAGS += -fexceptions
 APP_CPPFLAGS += -frtti += -Wno-error=format-security
 APP_CFLAGS = -Wno-error=format-security
