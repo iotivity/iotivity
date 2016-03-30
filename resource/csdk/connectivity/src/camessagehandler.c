839d642b7610d4d66ad464bb57de77b07d2ca59c
Abhishek Sharma <ce.abhishek@samsung.com>
2016-03-30 09:15:57 +0900

    [IOT-1043] [RI] Canceling observation using RESET is not working
    Related JIRA ticket : https://jira.iotivity.org/browse/IOT-1043
    
    Change-Id: I7f984fe8f649af0681352fed14f8a37d73b2eba9
    Signed-off-by: hyuna0213.jo <hyuna0213.jo@samsung.com>
    Signed-off-by: Abhishek Sharma <ce.abhishek@samsung.com>
    Reviewed-on: https://gerrit.iotivity.org/gerrit/7451
    Tested-by: jenkins-iotivity <jenkins-iotivity@opendaylight.org>
    Reviewed-by: Sungkyu Ko <sungkyu.ko@samsung.com>
    Reviewed-by: Gihong Kim <k1358.kim@samsung.com>
    Reviewed-by: Ashok Babu Channa <ashok.channa@samsung.com>

--- resource/csdk/connectivity/src/camessagehandler.c
+++ resource/csdk/connectivity/src/camessagehandler.c
@@ -781,7 +781,6 @@ static void CAReceivedPacketCallback(const CASecureEndpoint_t *sep,
             CARetransmissionReceivedData(&g_retransmissionContext, cadata->remoteEndpoint, pdu->hdr,
                                          pdu->length, &retransmissionPdu);
 
-#ifndef WITH_BWT
             // get token from saved data in retransmission list
             if (retransmissionPdu && CA_EMPTY == code)
             {
@@ -798,7 +797,6 @@ static void CAReceivedPacketCallback(const CASecureEndpoint_t *sep,
                     }
                 }
             }
-#endif
             OICFree(retransmissionPdu);
         }
     }
