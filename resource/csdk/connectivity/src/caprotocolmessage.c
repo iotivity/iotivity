854525234cfeb6a5ae85ef9384d3574fa1f25cec
jihwan.seo <jihwan.seo@samsung.com>
2016-04-28 11:34:37 +0900

    added filter related to CoAP option that is not supported.
    so many CoAP option is missed in iotivity.
    thur, i modified some filter code about it
    except for some option assciated with transport layer.
    
    - If-Match, If-None-Match is for PUT method
    - Location-Path, Location-Query is for POST Methos.
    
    Change-Id: I7e46932b086d1c4a87f337590415fe32525367c3
    Signed-off-by: jihwan.seo <jihwan.seo@samsung.com>
    Reviewed-on: https://gerrit.iotivity.org/gerrit/7955
    Tested-by: jenkins-iotivity <jenkins-iotivity@opendaylight.org>
    Reviewed-by: Jon A. Cruz <jon@joncruz.org>
    Signed-off-by: jihwan.seo <jihwan.seo@samsung.com>
    Reviewed-on: https://gerrit.iotivity.org/gerrit/8781
    Reviewed-by: Ashok Babu Channa <ashok.channa@samsung.com>

--- resource/csdk/connectivity/src/caprotocolmessage.c
+++ resource/csdk/connectivity/src/caprotocolmessage.c
@@ -669,7 +669,10 @@ uint32_t CAGetOptionCount(coap_opt_iterator_t opt_iter)
             && COAP_OPTION_BLOCK1 != opt_iter.type && COAP_OPTION_BLOCK2 != opt_iter.type
             && COAP_OPTION_SIZE1 != opt_iter.type && COAP_OPTION_SIZE2 != opt_iter.type
             && COAP_OPTION_CONTENT_FORMAT != opt_iter.type
-            && COAP_OPTION_ACCEPT != opt_iter.type)
+            && COAP_OPTION_ACCEPT != opt_iter.type
+            && COAP_OPTION_URI_HOST != opt_iter.type && COAP_OPTION_URI_PORT != opt_iter.type
+            && COAP_OPTION_ETAG != opt_iter.type && COAP_OPTION_MAXAGE != opt_iter.type
+            && COAP_OPTION_PROXY_URI != opt_iter.type && COAP_OPTION_PROXY_SCHEME != opt_iter.type)
         {
             count++;
         }
@@ -863,7 +866,17 @@ CAResult_t CAGetInfoFromPDU(const coap_pdu_t *pdu, const CAEndpoint_t *endpoint,
                     outInfo->acceptFormat = CA_FORMAT_UNSUPPORTED;
                 }
                 OIC_LOG_V(DEBUG, TAG, "option[%d] has an unsupported format [%d]",
-                        opt_iter.type, (uint8_t)buf[0]);
+                          opt_iter.type, (uint8_t)buf[0]);
+            }
+            else if (COAP_OPTION_URI_PORT == opt_iter.type ||
+                    COAP_OPTION_URI_HOST == opt_iter.type ||
+                    COAP_OPTION_ETAG == opt_iter.type ||
+                    COAP_OPTION_MAXAGE == opt_iter.type ||
+                    COAP_OPTION_PROXY_URI == opt_iter.type ||
+                    COAP_OPTION_PROXY_SCHEME== opt_iter.type)
+            {
+                OIC_LOG_V(INFO, TAG, "option[%d] has an unsupported format [%d]",
+                          opt_iter.type, (uint8_t)buf[0]);
             }
             else
             {
