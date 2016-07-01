6fba12a2676b22d07743b715deda56b12b976403
Philippe Coval <philippe.coval@osg.samsung.com>
2016-07-01 22:58:37 +0200

    ocpayload: in DMV remove undesired trailing separator
    Also add more tests cases too.
    
    Suggestion for improvements:
    
    Note the test uses a different message container to prevent ambiguity:
     same one could be used but accessed at next indice
     because MessageContainer::setPayload
     actually adds given payload to internal list.
    
    It would makes sense to rename method to MessageContainer::addPayload
     and eventually add a new different method like :
     MessageContainer::set(Payload) to reset list.
    
    Bug: https://jira.iotivity.org/browse/IOT-1174
    Change-Id: I9e7bf3c9eb7a311214e438ec2434d53b4ae0f313
    Signed-off-by: Philippe Coval <philippe.coval@osg.samsung.com>
    Reviewed-on: https://gerrit.iotivity.org/gerrit/9131
    Tested-by: jenkins-iotivity <jenkins-iotivity@opendaylight.org>
    Reviewed-by: Ziran Sun <ziran.sun@samsung.com>

--- resource/csdk/stack/src/ocpayload.c
+++ resource/csdk/stack/src/ocpayload.c
@@ -1334,15 +1334,17 @@ char* OCCreateString(const OCStringLL* ll)
     {
         len += strlen(it->value) + 1;
     }
+    len--; // renove trailing separator (just added above)
     str = (char*) malloc(len + 1);
     if (!str)
         return NULL;
 
     pos = str;
-    for (const OCStringLL *it = ll; it ; it = it->next )
+    const OCStringLL *it = ll;
+    while (it)
     {
-        sublen = strlen(it->value) + 1;
-        count = snprintf(pos, len + 1, "%s%c", it->value, CSV_SEPARATOR);
+        sublen = strlen(it->value);
+        count = snprintf(pos, len + 1, "%s", it->value);
         if (count<sublen)
         {
             free(str);
@@ -1350,6 +1352,14 @@ char* OCCreateString(const OCStringLL* ll)
         }
         len-=sublen;
         pos+=count;
+
+        it = it->next;
+        if (it)
+        {
+            *pos = CSV_SEPARATOR;
+            len--;
+            *(++pos) = '\0';
+       }
     }
 
     return str;
