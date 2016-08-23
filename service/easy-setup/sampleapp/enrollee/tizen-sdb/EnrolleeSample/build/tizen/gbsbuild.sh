43168e4e092ed5a362f58987dc7d0af9236f776d
sy01.youn <sy01.youn@samsung.com>
2016-08-24 07:57:59 +0900

    Remove unnecessary flags in script files when building iotivity & easy setup targeted to Tizen. Also remove unused tizen script files. In addtion, security related function is activated in Tizen Enrolle Sample App. (which was commented before) Tested on Tizen Mobile Z3, and it works well.
    Change-Id: If70424c31949aa22210b64c45003983ea56a8594
    Signed-off-by: sy01.youn <sy01.youn@samsung.com>
    Reviewed-on: https://gerrit.iotivity.org/gerrit/10833
    Tested-by: jenkins-iotivity <jenkins-iotivity@opendaylight.org>
    Reviewed-by: Madan Lanka <lanka.madan@samsung.com>

--- service/easy-setup/sampleapp/enrollee/tizen-sdb/EnrolleeSample/build/tizen/gbsbuild.sh
+++ service/easy-setup/sampleapp/enrollee/tizen-sdb/EnrolleeSample/build/tizen/gbsbuild.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 
-spec=`ls tools/tizen/*.spec`
+cur_dir="./service/easy-setup"
+spec=`ls ./service/easy-setup/sampleapp/enrollee/tizen-sdb/EnrolleeSample/packaging/*.spec`
 version=`rpm --query --queryformat '%{version}\n' --specfile $spec`
 
 name=`echo $name|cut -d" " -f 1`
@@ -15,13 +16,14 @@ sourcedir=`pwd`
 
 echo `pwd`
 
-# Clean tmp directory.
 rm -rf ./tmp
 
 # Create directory structure for GBS Build
 mkdir ./tmp
+mkdir ./tmp/con/
 mkdir ./tmp/extlibs/
 mkdir ./tmp/packaging
+mkdir ./tmp/logger
 cp -R ./build_common $sourcedir/tmp
 cp -R ./examples $sourcedir/tmp
 
@@ -32,7 +34,7 @@ rm -rf $sourcedir/tmp/extlibs/tinycbor/tinycbor/.git
 cp -R ./extlibs/cjson $sourcedir/tmp/extlibs
 cp -R ./extlibs/gtest $sourcedir/tmp/extlibs
 cp -R ./extlibs/tinydtls $sourcedir/tmp/extlibs
-cp -LR ./extlibs/sqlite3 $sourcedir/tmp/extlibs
+cp -R ./extlibs/sqlite3 $sourcedir/tmp/extlibs
 cp -R ./extlibs/timer $sourcedir/tmp/extlibs
 cp -R ./extlibs/rapidxml $sourcedir/tmp/extlibs
 cp -R ./resource $sourcedir/tmp
@@ -48,7 +50,6 @@ cp ./tools/tizen/*.rpm ./tmp
 cp ./tools/tizen/.gbs.conf ./tmp
 cp ./tools/tizen/*.rpm $sourcedir/tmp/service/easy-setup/sampleapp/enrollee/tizen-sdb/EnrolleeSample
 cp ./tools/tizen/.gbs.conf ./tmp/service/easy-setup/sampleapp/enrollee/tizen-sdb/EnrolleeSample
-
 cp -R $sourcedir/iotivity.pc.in $sourcedir/tmp
 
 cd $sourcedir/tmp
@@ -56,6 +57,9 @@ cd $sourcedir/tmp
 echo `pwd`
 rm -rf ./extlibs/tinycbor/tinycbor/.git*
 
+whoami
+
+# Build IoTivity
 # Initialize Git repository
 if [ ! -d .git ]; then
    git init ./
@@ -65,16 +69,39 @@ if [ ! -d .git ]; then
    git commit -m "Initial commit"
 fi
 
+buildoption="--define 'TARGET_TRANSPORT '$1 --define 'SECURED '$2 --define 'ROUTING '$3 --define 'RELEASE '$4 --define 'LOGGING '$5 --define 'ES_TARGET_ENROLLEE '$6"
 echo "Calling core gbs build command"
-gbscommand="gbs build -A armv7l -B ~/GBS-ROOT-OIC --include-all --repository ./"
+gbscommand="gbs build -A armv7l -B ~/GBS-ROOT-OIC $buildoption --include-all --repository ./"
 echo $gbscommand
 if eval $gbscommand; then
-    echo "Build is successful"
+   echo "Core build is successful"
 else
-    echo "Build failed!"
-    exit 1
+   echo "Core build failed."
+   cd $sourcedir
+   rm -rf $sourcedir/tmp
+   exit 1
 fi
 
+# Build Enrollee Sample App
+cd service/easy-setup/sampleapp/enrollee/tizen-sdb/EnrolleeSample
+echo `pwd`
+# Initialize Git repository
+if [ ! -d .git ]; then
+  git init ./
+  git config user.email "you@example.com"
+  git config user.name "Your Name"
+  git add ./
+  git commit -m "Initial commit"
+fi
+echo "Calling sample gbs build command"
+gbscommand="gbs build -A armv7l -B ~/GBS-ROOT-OIC $buildoption --include-all --repository ./"
+echo $gbscommand
+if eval $gbscommand; then
+  echo "Sample build is successful"
+else
+  echo "Sample build is failed."
+  exit 1
+fi
 rm -rf tmp
 cd $sourcedir
 rm -rf $sourcedir/tmp
