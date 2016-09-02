0d39ddf63b864ed0394ac03722c8f7b881520d15
jaesick.shin <jaesick.shin@samsung.com>
2016-09-02 15:17:59 +0900

    change strcpy to strncpy.
    strcpy function is unsafe.
    
    Change-Id: Ifd679a4b9804861da409d4318b69502992d8ef90
    Signed-off-by: jaesick.shin <jaesick.shin@samsung.com>
    Reviewed-on: https://gerrit.iotivity.org/gerrit/11327
    Reviewed-by: Uze Choi <uzchoi@samsung.com>
    Tested-by: Uze Choi <uzchoi@samsung.com>

--- service/notification/unittest/NSProviderTest.cpp
+++ service/notification/unittest/NSProviderTest.cpp
@@ -123,9 +123,9 @@ protected:
 
             g_isStartedStack = true;
 
-            strcpy(g_title, "Title");
-            strcpy(g_body, "ContentText");
-            strcpy(g_sourceName, "OIC");
+            strncpy(g_title, "Title", strlen("Title"));
+            strncpy(g_body, "ContentText", strlen("ContentText"));
+            strncpy(g_sourceName, "OIC", strlen("OIC"));
         }
 
     }
