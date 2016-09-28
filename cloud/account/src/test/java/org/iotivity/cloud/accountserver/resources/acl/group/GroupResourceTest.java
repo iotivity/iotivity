d6a131213271962d614228cffe68ac51cd6ab685
yeonghun.nam <yeonghun.nam@samsung.com>
2016-09-28 09:03:53 +0900

    Fixed account server TC
    1. DB reset for each test case
    
    Change-Id: I7cc9d46af2ffe6136b13231d9b0e015ee6799dcf
    Signed-off-by: yeonghun.nam <yeonghun.nam@samsung.com>
    Reviewed-on: https://gerrit.iotivity.org/gerrit/12373
    Tested-by: jenkins-iotivity <jenkins-iotivity@opendaylight.org>
    Reviewed-by: Jee Hyeok Kim <jihyeok13.kim@samsung.com>
    (cherry picked from commit c8ea7c0831a5a268b3a4ac4fda81d3b420121645)
    Reviewed-on: https://gerrit.iotivity.org/gerrit/16547
    Tested-by: jenkins-iotivity <jenkins@iotivity.org>

--- cloud/account/src/test/java/org/iotivity/cloud/accountserver/resources/acl/group/GroupResourceTest.java
+++ cloud/account/src/test/java/org/iotivity/cloud/accountserver/resources/acl/group/GroupResourceTest.java
@@ -121,6 +121,14 @@ public class GroupResourceTest {
         mongoDB.createTable(Constants.ACE_TABLE);
     }
 
+    @After
+    public void resetAccountDatabase() throws Exception {
+        MongoDB mongoDB = new MongoDB(Constants.DB_NAME);
+        mongoDB.createTable(Constants.USER_TABLE);
+        mongoDB.createTable(Constants.TOKEN_TABLE);
+        mongoDB.createTable(Constants.GROUP_TABLE);
+    }
+
     @Test
     public void testCreateGroup() throws Exception {
         getTestMethodName();
