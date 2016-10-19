e589beb7cefc15365f903e98ba0ef16f1cafc4dd
bg.chun <bg.chun@samsung.com>
2016-10-19 16:30:48 +0900

    Add client side logic when obs request failed.
    Add client side logic and fix example.
    
    Change-Id: Iffedfddde5906a5d27fd7cbad0176197730d89e3
    Signed-off-by: bg.chun <bg.chun@samsung.com>
    Reviewed-on: https://gerrit.iotivity.org/gerrit/13783
    Tested-by: jenkins-iotivity <jenkins-iotivity@opendaylight.org>
    Reviewed-by: Dan Mihai <Daniel.Mihai@microsoft.com>
    Reviewed-by: Ashok Babu Channa <ashok.channa@samsung.com>

--- resource/csdk/stack/src/ocstack.c
+++ resource/csdk/stack/src/ocstack.c
@@ -1228,6 +1228,11 @@ void OCHandleResponse(const CAEndpoint_t* endPoint, const CAResponseInfo_t* resp
     if(cbNode)
     {
         OIC_LOG(INFO, TAG, "There is a cbNode associated with the response token");
+        uint32_t obsHeaderOpt = OC_OBSERVE_NO_OPTION;
+        uint8_t tempNumOpt = responseInfo->info.numOptions;
+        GetObserveHeaderOption(&obsHeaderOpt, responseInfo->info.options,
+                               &tempNumOpt);
+
         if(responseInfo->result == CA_EMPTY)
         {
             OIC_LOG(INFO, TAG, "Receiving A ACK/RESET for this token");
@@ -1257,6 +1262,25 @@ void OCHandleResponse(const CAEndpoint_t* endPoint, const CAResponseInfo_t* resp
             response.result = CAResponseToOCStackResult(responseInfo->result);
             cbNode->callBack(cbNode->context,
                     cbNode->handle, &response);
+            FindAndDeleteClientCB(cbNode);
+        }
+        else if ((cbNode->method == OC_REST_OBSERVE || cbNode->method == OC_REST_OBSERVE_ALL)
+                && (responseInfo->result == CA_CONTENT)
+                && (obsHeaderOpt == OC_OBSERVE_NO_OPTION))
+        {
+            OCClientResponse response =
+                {.devAddr = {.adapter = OC_DEFAULT_ADAPTER}};
+            CopyEndpointToDevAddr(endPoint, &response.devAddr);
+            FixUpClientResponse(&response);
+            response.resourceUri = responseInfo->info.resourceUri;
+            memcpy(response.identity.id, responseInfo->info.identity.id,
+                                    sizeof (response.identity.id));
+            response.identity.id_length = responseInfo->info.identity.id_length;
+            response.result = OC_STACK_UNAUTHORIZED_REQ;
+
+            cbNode->callBack(cbNode->context,
+                             cbNode->handle,
+                             &response);
             FindAndDeleteClientCB(cbNode);
         }
         else
