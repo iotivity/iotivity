3841728c50173c3528d9d24038ad4ca59e70b9a6
Jihun Ha <jihun.ha@samsung.com>
2016-10-27 16:39:25 +0900

    Add more descriptive log messages in easy setup
    This patch is for removing mis-used log messages and
    adding one for more convenient debugging.
    
    Change-Id: I354292210a18e16e74d8ae3c0d66b91a90f31ab9
    Signed-off-by: Jihun Ha <jihun.ha@samsung.com>
    Reviewed-on: https://gerrit.iotivity.org/gerrit/12963
    Tested-by: jenkins-iotivity <jenkins-iotivity@opendaylight.org>
    Reviewed-by: Heewon Park <h_w.park@samsung.com>
    Reviewed-by: Uze Choi <uzchoi@samsung.com>
    (cherry picked from commit 13f06a192eac3c8fd6ef76fbfba9d64eb2f8e78f)
    Reviewed-on: https://gerrit.iotivity.org/gerrit/13745
    Reviewed-by: Senthil Kumar G S <senthil.gs@samsung.com>
    Reviewed-by: Madan Lanka <lanka.madan@samsung.com>

--- service/easy-setup/mediator/richsdk/src/EnrolleeSecurity.h
+++ service/easy-setup/mediator/richsdk/src/EnrolleeSecurity.h
@@ -32,7 +32,7 @@ namespace OIC
 {
     namespace Service
     {
-        #define ENROLEE_SECURITY_TAG "ENROLLEE_SECURITY"
+        #define ENROLEE_SECURITY_TAG "ES_ENROLLEE_SECURITY"
         #define UUID_SIZE (16)
         #define UUID_STRING_SIZE (37)
 
