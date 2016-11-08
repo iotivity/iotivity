fc70845bc97acf3d8d03ce172de8578b2d023049
Habib Virji <habib.virji@samsung.com>
2016-11-08 16:43:48 +0000

    [JIRA-1134] Fix mem leak in HandleVirtualResource
    - VERIFY_NON_NULL to exit by cleaning the mem already allocated.
    - InterfaceQuery which wa being assigned is not done, and instead if no interfaceQuery it is considered as oic.if.ll.
    - Moved out rd resource search out of function,
    - Baseline information handling has been updated to be handled separately.
    
    https://jira.iotivity.org/browse/IOT-1134
    
    Change-Id: Ie894284e4c67a7bbd4bbaa52801416a0c8dab778
    Signed-off-by: Habib Virji <habib.virji@samsung.com>
    Reviewed-on: https://gerrit.iotivity.org/gerrit/9279
    Tested-by: jenkins-iotivity <jenkins-iotivity@opendaylight.org>
    Reviewed-by: David Antler <david.a.antler@intel.com>
    (cherry picked from commit 02ec9d7a594a13827c844d5e205927e6b7afab7d)
    Reviewed-on: https://gerrit.iotivity.org/gerrit/12283
    Reviewed-by: Phil Coval <philippe.coval@osg.samsung.com>

--- resource/csdk/stack/src/ocstack.c
+++ resource/csdk/stack/src/ocstack.c
@@ -1662,6 +1662,8 @@ OCStackResult SendDirectStackResponse(const CAEndpoint_t* endPoint, const uint16
     if(OC_STACK_OK != result)
     {
         OIC_LOG_V(ERROR, TAG, "Add routing option failed [%d]", result);
+        OICFree (respInfo.info.resourceUri);
+        OICFree (respInfo.info.options);
         return result;
     }
     if (doPost)
@@ -1686,6 +1688,9 @@ OCStackResult SendDirectStackResponse(const CAEndpoint_t* endPoint, const uint16
             if (NULL == reqInfo.info.options)
             {
                 OIC_LOG(ERROR, TAG, "Calloc failed");
+                OICFree (reqInfo.info.resourceUri);
+                OICFree (respInfo.info.resourceUri);
+                OICFree (respInfo.info.options);
                 return OC_STACK_NO_MEMORY;
             }
             memcpy (reqInfo.info.options, respInfo.info.options,
