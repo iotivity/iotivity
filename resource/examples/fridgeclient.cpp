5910047b297218f49079807b44ca3d3720581f7f
jihwan.seo <jihwan.seo@samsung.com>
2016-11-16 15:08:17 +0900

    fix Sample App related getPlatformInfo/getDeviceInfo
    Change-Id: I07e2e0e24241f0c5dd53259f8333eccc722d4e39
    Signed-off-by: jihwan.seo <jihwan.seo@samsung.com>
    Reviewed-on: https://gerrit.iotivity.org/gerrit/14405
    Tested-by: jenkins-iotivity <jenkins-iotivity@opendaylight.org>
    Reviewed-by: Larry Sachs <larry.j.sachs@intel.com>
    Reviewed-by: Rick Bell <richard.s.bell@intel.com>
    Reviewed-by: Jaehong Jo <jaehong.jo@samsung.com>
    Reviewed-by: Ziran Sun <ziran.sun@samsung.com>

--- resource/examples/fridgeclient.cpp
+++ resource/examples/fridgeclient.cpp
@@ -136,38 +136,43 @@ class ClientFridge
 
         OCStackResult ret;
 
-        std::cout << "Querying for platform information... " << std::endl;
-
-        ret = OCPlatform::getPlatformInfo(resource->host(), platformDiscoveryURI,
-                                          resource->connectivityType(),
-                                          std::bind(&ClientFridge::receivedPlatformInfo,
-                                                    this, PH::_1));
-
-        if (ret == OC_STACK_OK)
+        if (0 == strcmp(resource->uri().c_str(), platformDiscoveryURI.c_str()))
         {
-            std::cout << "Get platform information is done." << std::endl;
-        }
-        else
-        {
-            std::cout << "Get platform information failed." << std::endl;
-        }
+            std::cout << "Querying for platform information... " << std::endl;
 
-        std::cout << "Querying for device information... " << std::endl;
+            ret = OCPlatform::getPlatformInfo(resource->host(), platformDiscoveryURI,
+                                              resource->connectivityType(),
+                                              std::bind(&ClientFridge::receivedPlatformInfo,
+                                                        this, PH::_1));
 
-        ret = OCPlatform::getDeviceInfo(resource->host(), deviceDiscoveryURI,
-                                        resource->connectivityType(),
-                                        std::bind(&ClientFridge::receivedDeviceInfo,
-                                                  this, PH::_1));
-
-        if (ret == OC_STACK_OK)
-        {
-            std::cout << "Getting device information is done." << std::endl;
-        }
-        else
-        {
-            std::cout << "Getting device information failed." << std::endl;
+            if (ret == OC_STACK_OK)
+            {
+                std::cout << "Get platform information is done." << std::endl;
+            }
+            else
+            {
+                std::cout << "Get platform information failed." << std::endl;
+            }
         }
 
+        if (0 == strcmp(resource->uri().c_str(), deviceDiscoveryURI.c_str()))
+        {
+            std::cout << "Querying for device information... " << std::endl;
+
+            ret = OCPlatform::getDeviceInfo(resource->host(), deviceDiscoveryURI,
+                                            resource->connectivityType(),
+                                            std::bind(&ClientFridge::receivedDeviceInfo,
+                                                      this, PH::_1));
+
+            if (ret == OC_STACK_OK)
+            {
+                std::cout << "Getting device information is done." << std::endl;
+            }
+            else
+            {
+                std::cout << "Getting device information failed." << std::endl;
+            }
+        }
         // we have now found a resource, so lets create a few resource objects
         // for the other resources that we KNOW are associated with the intel.fridge
         // server, and query them.
