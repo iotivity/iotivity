c253df96dddf8f45b34bccf3dfea15b528f7bc1f
hyuna0213.jo <hyuna0213.jo@samsung.com>
2016-11-21 08:51:44 +0900

    Fix windows build failed about incompatible types in jenkins
    - resolve incompatible types error to build windows
    
    Change-Id: I5095bb7de066eb966316812d57728361c685a811
    Signed-off-by: hyuna0213.jo <hyuna0213.jo@samsung.com>
    Reviewed-on: https://gerrit.iotivity.org/gerrit/14533
    Tested-by: jenkins-iotivity <jenkins-iotivity@opendaylight.org>
    Reviewed-by: Dan Mihai <Daniel.Mihai@microsoft.com>
    Reviewed-by: Dave Thaler <dthaler@microsoft.com>
    Reviewed-by: Ashok Babu Channa <ashok.channa@samsung.com>
    (cherry picked from commit 54e9021921fece152c8c87e3d25f28b0bc0f5608)
    Reviewed-on: https://gerrit.iotivity.org/gerrit/14559
    Reviewed-by: Phil Coval <philippe.coval@osg.samsung.com>
    Reviewed-by: Jaewook Jung <jw0213.jung@samsung.com>
    Reviewed-by: Jaehong Jo <jaehong.jo@samsung.com>

--- resource/csdk/connectivity/src/ip_adapter/caipadapter.c
+++ resource/csdk/connectivity/src/ip_adapter/caipadapter.c
@@ -77,10 +77,10 @@ static CAAdapterChangeCallback g_networkChangeCallback = NULL;
 static CAErrorHandleCallback g_errorCallback = NULL;
 
 static void CAIPPacketReceivedCB(const CASecureEndpoint_t *endpoint,
-                                 const void *data, uint32_t dataLength);
+                                 const void *data, size_t dataLength);
 #ifdef __WITH_DTLS__
-static int32_t CAIPPacketSendCB(CAEndpoint_t *endpoint,
-                                const void *data, uint32_t dataLength);
+static ssize_t CAIPPacketSendCB(CAEndpoint_t *endpoint,
+                                const void *data, size_t dataLength);
 #endif
 
 #ifndef SINGLE_THREAD
@@ -151,7 +151,7 @@ void CAIPAdapterHandler(CATransportAdapter_t adapter, CANetworkStatus_t status)
 }
 
 #ifdef __WITH_DTLS__
-static int32_t CAIPPacketSendCB(CAEndpoint_t *endpoint, const void *data, uint32_t dataLength)
+static ssize_t CAIPPacketSendCB(CAEndpoint_t *endpoint, const void *data, size_t dataLength)
 {
     VERIFY_NON_NULL_RET(endpoint, TAG, "endpoint is NULL", -1);
     VERIFY_NON_NULL_RET(data, TAG, "data is NULL", -1);
@@ -163,7 +163,7 @@ static int32_t CAIPPacketSendCB(CAEndpoint_t *endpoint, const void *data, uint32
 
 
 void CAIPPacketReceivedCB(const CASecureEndpoint_t *sep, const void *data,
-                          uint32_t dataLength)
+                          size_t dataLength)
 {
     VERIFY_NON_NULL_VOID(sep, TAG, "sep is NULL");
     VERIFY_NON_NULL_VOID(data, TAG, "data is NULL");
