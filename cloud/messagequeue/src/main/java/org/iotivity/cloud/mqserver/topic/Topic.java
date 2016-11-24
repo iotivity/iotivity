9c1eca76b4e14485748e219f4add9bcb125f68a3
Jung Seungho <shonest.jung@samsung.com>
2016-11-24 13:11:55 +0900

    [IOT-1599] fix message publish in MQ
    removed logic for checking 'message' property
    https://jira.iotivity.org/browse/IOT-1599
    
    Change-Id: I1edc3eb68d97156ce3befe44205e2d1ff6006b7c
    Signed-off-by: Jung Seungho <shonest.jung@samsung.com>
    Reviewed-on: https://gerrit.iotivity.org/gerrit/14715
    Reviewed-by: Uze Choi <uzchoi@samsung.com>
    Tested-by: jenkins-iotivity <jenkins-iotivity@opendaylight.org>
    Reviewed-by: Jee Hyeok Kim <jihyeok13.kim@samsung.com>
    (cherry picked from commit 647b538360333cdf408eba9e4a62b37dfef1cc71)
    Reviewed-on: https://gerrit.iotivity.org/gerrit/14725

--- cloud/messagequeue/src/main/java/org/iotivity/cloud/mqserver/topic/Topic.java
+++ cloud/messagequeue/src/main/java/org/iotivity/cloud/mqserver/topic/Topic.java
@@ -118,8 +118,8 @@ public class Topic {
      */
     public IResponse handleCreateSubtopic(IRequest request) {
 
-        String newTopicName = request.getUriPathSegments().get(
-                request.getUriPathSegments().size() - 1);
+        String newTopicName = request.getUriPathSegments()
+                .get(request.getUriPathSegments().size() - 1);
 
         String newTopicType = new String();
 
@@ -206,8 +206,8 @@ public class Topic {
         }
 
         synchronized (mSubscribers) {
-            mSubscribers.put(request.getRequestId(), new TopicSubscriber(
-                    srcDevice, request));
+            mSubscribers.put(request.getRequestId(),
+                    new TopicSubscriber(srcDevice, request));
         }
 
         return MessageBuilder.createResponse(request, ResponseStatus.CONTENT,
@@ -226,8 +226,8 @@ public class Topic {
 
         synchronized (mSubscribers) {
 
-            TopicSubscriber subscriber = mSubscribers.get(request
-                    .getRequestId());
+            TopicSubscriber subscriber = mSubscribers
+                    .get(request.getRequestId());
 
             mSubscribers.remove(subscriber.mRequest.getRequestId());
 
@@ -260,10 +260,8 @@ public class Topic {
         HashMap<String, Object> message = mCbor.parsePayloadFromCbor(payload,
                 HashMap.class);
 
-        if (message == null
-                || message.containsKey(Constants.MQ_MESSAGE) == false) {
-            throw new PreconditionFailedException(
-                    "message field is not included");
+        if (message == null || message.isEmpty()) {
+            throw new PreconditionFailedException("message is not included");
         }
 
         if (mKafkaProducerOperator.publishMessage(payload) == false) {
@@ -333,8 +331,8 @@ public class Topic {
         synchronized (mSubscribers) {
             for (TopicSubscriber subscriber : mSubscribers.values()) {
 
-                subscriber.mSubscriber.sendResponse(MessageBuilder
-                        .createResponse(subscriber.mRequest,
+                subscriber.mSubscriber.sendResponse(
+                        MessageBuilder.createResponse(subscriber.mRequest,
                                 ResponseStatus.CONTENT,
                                 ContentFormat.APPLICATION_CBOR, mLatestData));
             }
