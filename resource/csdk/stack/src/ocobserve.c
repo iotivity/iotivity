7db33c3f5075ba082ade80c9f106a9c49e1a0a85
Habib Virji <habib.virji@samsung.com>
2016-12-12 12:20:55 +0000

    Double  free of OCServerRequest
    OCServerRequest was deleted once in SendListObserverNotification and HandleSingleResponse.
    
    Change-Id: I2964a6a65d760369afaf3e1325eae825d4ce3345
    Signed-off-by: Habib Virji <habib.virji@samsung.com>
    Reviewed-on: https://gerrit.iotivity.org/gerrit/15503
    Tested-by: jenkins-iotivity <jenkins-iotivity@opendaylight.org>
    Reviewed-by: Uze Choi <uzchoi@samsung.com>
    Reviewed-by: Ziran Sun <ziran.sun@samsung.com>

--- resource/csdk/stack/src/ocobserve.c
+++ resource/csdk/stack/src/ocobserve.c
@@ -317,7 +317,6 @@ OCStackResult SendListObserverNotification (OCResource * resource,
                             numSentNotification++;
 
                             OICFree(ehResponse.payload);
-                            FindAndDeleteServerRequest(request);
                         }
                         else
                         {
@@ -682,4 +681,3 @@ GetObserveHeaderOption (uint32_t * observationOption,
     }
     return OC_STACK_OK;
 }
-
