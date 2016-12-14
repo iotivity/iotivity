50bede1bcb83ba468bf6e975d34d1903ced65636
js126.lee <js126.lee@samsung.com>
2016-12-14 13:40:09 +0900

    Fixed bug of gernerating pin during randompin OTM
    Bug : During generating randompin, the bound of curIndex is one more.
    
    Patch 1: Upload patch.
    
    Change-Id: I0c6416adaa8f0fbd8eeb936a0cc17b64047b9222
    Signed-off-by: js126.lee <js126.lee@samsung.com>
    Reviewed-on: https://gerrit.iotivity.org/gerrit/15579
    Reviewed-by: Jongmin Choi <jminl.choi@samsung.com>
    Tested-by: jenkins-iotivity <jenkins-iotivity@opendaylight.org>
    Reviewed-by: Randeep Singh <randeep.s@samsung.com>
    (cherry picked from commit 78fdef84ed34707fac4bf3a6f9abf20803046f88)
    Reviewed-on: https://gerrit.iotivity.org/gerrit/15583

--- resource/csdk/security/src/oxmpincommon.c
+++ resource/csdk/security/src/oxmpincommon.c
@@ -151,6 +151,10 @@ static char GenerateRandomPinElement(OicSecPinType_t pinType)
     {
         return defaultRetValue;
     }
+    else
+    {
+        curIndex -= 1;
+    }
 
     return allowedCharacters[OCGetRandomRange(0, curIndex)];
 }
