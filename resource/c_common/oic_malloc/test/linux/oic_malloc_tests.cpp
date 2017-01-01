f2f73c75fdb2fba28b0a2c02f5308ac54bb0606e
Dan Mihai <Daniel.Mihai@microsoft.com>
2017-01-01 02:46:39 -0800

    Fix build break.
    Fix conflict between recent commits
    1f885a36f6258373bef2c08c46d12b3b9d615551
    d4f573ea1f2a0089fb7d53980ac39c39673391cb
    
    Change-Id: Ic740ab0a8c57810b4a5f0841a42905ae09052d88
    Signed-off-by: Dan Mihai <Daniel.Mihai@microsoft.com>
    Reviewed-on: https://gerrit.iotivity.org/gerrit/16031
    Tested-by: jenkins-iotivity <jenkins-iotivity@opendaylight.org>

--- resource/c_common/oic_malloc/test/linux/oic_malloc_tests.cpp
+++ resource/c_common/oic_malloc/test/linux/oic_malloc_tests.cpp
@@ -162,7 +162,7 @@ TEST_P(OICCallocFailTests, shouldFailOICCalloc)
 TEST(OICFreeAndSetToNull, FreeAndSetToNullPass1)
 {
     // Try to deallocate a block of memory
-    pBuffer = (uint8_t *)OICCalloc(1, 1);
+    uint8_t* pBuffer = (uint8_t *)OICCalloc(1, 1);
     OICFreeAndSetToNull((void**)&pBuffer);
     EXPECT_TRUE(NULL == pBuffer);
 }
@@ -170,7 +170,7 @@ TEST(OICFreeAndSetToNull, FreeAndSetToNullPass1)
 TEST(OICFreeAndSetToNull, FreeAndSetToNullPass2)
 {
     // Try to deallocate a block of NULL
-    pBuffer = NULL;
+    uint8_t* pBuffer = NULL;
     OICFreeAndSetToNull((void**)&pBuffer);
     EXPECT_TRUE(NULL == pBuffer);
 }
