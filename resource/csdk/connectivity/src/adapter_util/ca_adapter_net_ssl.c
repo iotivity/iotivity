f6aa06556591d0fbfb056788457123d69fb7b893
Chul Lee <chuls.lee@samsung.com>
2017-01-11 16:59:02 +0900

    Add NULL check in StartRetransmit function.
    Change-Id: Ia7772c1cd574be8b001a43e7b6b482663dc2a2b4
    Signed-off-by: Chul Lee <chuls.lee@samsung.com>
    Reviewed-on: https://gerrit.iotivity.org/gerrit/16311
    Tested-by: jenkins-iotivity <jenkins-iotivity@opendaylight.org>
    Reviewed-by: Kevin Kane <kkane@microsoft.com>
    Reviewed-by: Greg Zaverucha <gregz@microsoft.com>
    Reviewed-by: Joonghwan Lee <jh05.lee@samsung.com>
    Reviewed-by: Randeep Singh <randeep.s@samsung.com>

--- resource/csdk/connectivity/src/adapter_util/ca_adapter_net_ssl.c
+++ resource/csdk/connectivity/src/adapter_util/ca_adapter_net_ssl.c
@@ -1425,7 +1425,7 @@ static int StartRetransmit()
         {
             tep = (SslEndPoint_t *) u_arraylist_get(g_caSslContext->peerList, listIndex);
             if (NULL == tep
-                || MBEDTLS_SSL_TRANSPORT_STREAM == tep->ssl.conf->transport
+                || (tep->ssl.conf && MBEDTLS_SSL_TRANSPORT_STREAM == tep->ssl.conf->transport)
                 || MBEDTLS_SSL_HANDSHAKE_OVER == tep->ssl.state)
             {
                 continue;
@@ -1501,7 +1501,7 @@ CAResult_t CAinitSslAdapter()
     mbedtls_entropy_init(&g_caSslContext->entropy);
     mbedtls_ctr_drbg_init(&g_caSslContext->rnd);
     if(0 != mbedtls_ctr_drbg_seed(&g_caSslContext->rnd, mbedtls_entropy_func,
-                                  &g_caSslContext->entropy, 
+                                  &g_caSslContext->entropy,
                                   (const unsigned char*) PERSONALIZATION_STRING, sizeof(PERSONALIZATION_STRING)))
     {
         OIC_LOG(ERROR, NET_SSL_TAG, "Seed initialization failed!");
