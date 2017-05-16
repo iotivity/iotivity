iotivity/iotivity

commit 05671f38527acd30ff5a761b3c0a7cde933a0b17

[IOT-2251] Resolve getValue() & setValue() APIs for argument string, Attribute Value getValue() & setValue() APIs returned "no matching function argument" while being callled with the argument: string, AttributeValue Previously this worked fine, this problem is being generated because of the commit: https://gerrit.iotivity.org/gerrit/#/c/19585
Change-Id: Ibcf7d6f1d7e65a32ee7113ddaec958ab8073aa07
Signed-off-by: Mushfiqu Islam Antu <i.mushfiq@samsung.com>
Reviewed-on: https://gerrit.iotivity.org/gerrit/19901
Tested-by: jenkins-iotivity <jenkins@iotivity.org>
Reviewed-by: Uze Choi <uzchoi@samsung.com>
Reviewed-by: Ashok Babu Channa <ashok.channa@samsung.com>

resource/include/AttributeValue.h
@@ -49,6 +49,7 @@ namespace OC
     static const int AttributeValueNullIndex = 0;
     typedef boost::variant<
         NullType, // Note: this handles the null-type and must match the above static const
+        int,
         int64_t,
         double,
         bool,

resource/include/OCRepresentation.h
@@ -116,6 +116,7 @@ namespace OC
                 std::is_same<typename std::remove_cv<typename std::remove_reference<T>::type>::type, std::vector<std::vector<OCByteString>>>::value ||
                 std::is_same<typename std::remove_cv<typename std::remove_reference<T>::type>::type, std::vector<std::vector<std::vector<OCByteString>>>>::value ||
                 std::is_same<typename std::remove_cv<typename std::remove_reference<T>::type>::type, std::vector<uint8_t>>::value ||
+                std::is_same<typename std::remove_cv<typename std::remove_reference<T>::type>::type, AttributeValue>::value ||
                 std::is_same<typename std::remove_cv<typename std::remove_reference<T>::type>::type, NullType>::value;
         };
 

commit 94b6435fa932968dc0ba07550410d51ca5dca01d

Fix Windows build break
Increase the default compiler limit. Error started with recent change
05671f38527acd30ff5a761b3c0a7cde933a0b17.

Fixing:

fatal error C1128: number of sections exceeded object file format
limit: compile with /bigobj.

Change-Id: I1e4c1284c7fb7057a14355d7362397ed2919c37d
Signed-off-by: Dan Mihai <Daniel.Mihai@microsoft.com>
Reviewed-on: https://gerrit.iotivity.org/gerrit/19961
Reviewed-by: Dave Thaler <dthaler@microsoft.com>
Reviewed-by: George Nash <george.nash@intel.com>
Tested-by: jenkins-iotivity <jenkins@iotivity.org>

resource/unittests/SConscript
@@ -52,6 +52,11 @@ if target_os in ['windows']:
     # Non-Windows platforms find the boost headers under /usr/include or similar path.
     unittests_env.AppendUnique(CPPPATH = ['#extlibs/boost/boost'])
     unittests_env.AppendUnique(LIBS = ['sqlite3'])
+
+    if unittests_env['CC'] == 'cl':
+        # Fix for: resource\unittests\OCRepresentationEncodingTest.cpp "fatal error C1128:
+        # number of sections exceeded object file format limit: compile with /bigobj"
+        unittests_env.AppendUnique(CCFLAGS=['/bigobj'])
 elif target_os in ['linux', 'tizen']:
     unittests_env.ParseConfig('pkg-config --cflags --libs sqlite3 gobject-2.0 gio-2.0 glib-2.0')
 