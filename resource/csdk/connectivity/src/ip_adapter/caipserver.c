iotivity/iotivity

commit e3a3f65569f15fdf711c9d0a3123e6951f6499ba

[IOT-2063] Enable endpoints filtering in CAIPGetInterfaceInformation()
https://gerrit.iotivity.org/gerrit/#/c/19355/ was modified to make
changes for all platforms.

Change-Id: Ib653daaad453556354b108c673b34c06a197c5f9
Signed-off-by: hyuna0213.jo <hyuna0213.jo@samsung.com>
Reviewed-on: https://gerrit.iotivity.org/gerrit/19821
Tested-by: jenkins-iotivity <jenkins@iotivity.org>
Reviewed-by: Jaehong Jo <jaehong.jo@samsung.com>
Reviewed-by: Uze Choi <uzchoi@samsung.com>
Tested-by: Uze Choi <uzchoi@samsung.com>

resource/csdk/connectivity/src/ip_adapter/caipserver.c
@@ -1543,8 +1543,28 @@ CAResult_t CAGetIPInterfaceInformation(CAEndpoint_t **info, size_t *size)
 #endif
 
     size_t interfaces = u_arraylist_length(iflist);
-    size_t totalEndpoints = interfaces * endpointsPerInterface;
+    for (size_t i = 0; i < interfaces; i++)
+    {
+        CAInterface_t *ifitem = (CAInterface_t *)u_arraylist_get(iflist, i);
+        if (!ifitem)
+        {
+            continue;
+        }
 
+        if ((ifitem->family == AF_INET6 && !caglobals.ip.ipv6enabled) ||
+            (ifitem->family == AF_INET && !caglobals.ip.ipv4enabled))
+        {
+            interfaces--;
+        }
+    }
+
+    if (!interfaces)
+    {
+        OIC_LOG(DEBUG, TAG, "network interface size is zero");
+        return CA_STATUS_OK;
+    }
+
+    size_t totalEndpoints = interfaces * endpointsPerInterface;
     CAEndpoint_t *eps = (CAEndpoint_t *)OICCalloc(totalEndpoints, sizeof (CAEndpoint_t));
     if (!eps)
     {
@@ -1553,10 +1573,16 @@ CAResult_t CAGetIPInterfaceInformation(CAEndpoint_t **info, size_t *size)
         return CA_MEMORY_ALLOC_FAILED;
     }
 
-    for (size_t i = 0, j = 0; i < interfaces; i++)
+    for (size_t i = 0, j = 0; i < u_arraylist_length(iflist); i++)
     {
         CAInterface_t *ifitem = (CAInterface_t *)u_arraylist_get(iflist, i);
-        if(!ifitem)
+        if (!ifitem)
+        {
+            continue;
+        }
+
+        if ((ifitem->family == AF_INET6 && !caglobals.ip.ipv6enabled) ||
+            (ifitem->family == AF_INET && !caglobals.ip.ipv4enabled))
         {
             continue;
         }

resource/csdk/connectivity/src/tcp_adapter/catcpserver.c
@@ -1320,8 +1320,28 @@ CAResult_t CAGetTCPInterfaceInformation(CAEndpoint_t **info, size_t *size)
 #endif
 
     size_t interfaces = u_arraylist_length(iflist);
-    size_t totalEndpoints = interfaces * endpointsPerInterface;
+    for (size_t i = 0; i < interfaces; i++)
+    {
+        CAInterface_t *ifitem = (CAInterface_t *)u_arraylist_get(iflist, i);
+        if (!ifitem)
+        {
+            continue;
+        }
 
+        if ((ifitem->family == AF_INET6 && !caglobals.ip.ipv6enabled) ||
+            (ifitem->family == AF_INET && !caglobals.ip.ipv4enabled))
+        {
+            interfaces--;
+        }
+    }
+
+    if (!interfaces)
+    {
+        OIC_LOG(DEBUG, TAG, "network interface size is zero");
+        return CA_STATUS_OK;
+    }
+
+    size_t totalEndpoints = interfaces * endpointsPerInterface;
     CAEndpoint_t *ep = (CAEndpoint_t *)OICCalloc(totalEndpoints, sizeof (CAEndpoint_t));
     if (!ep)
     {
@@ -1330,10 +1350,16 @@ CAResult_t CAGetTCPInterfaceInformation(CAEndpoint_t **info, size_t *size)
         return CA_MEMORY_ALLOC_FAILED;
     }
 
-    for (size_t i = 0, j = 0; i < interfaces; i++)
+    for (size_t i = 0, j = 0; i < u_arraylist_length(iflist); i++)
     {
         CAInterface_t *ifitem = (CAInterface_t *)u_arraylist_get(iflist, i);
         if (!ifitem)
+        {
+            continue;
+        }
+
+        if ((ifitem->family == AF_INET6 && !caglobals.ip.ipv6enabled) ||
+            (ifitem->family == AF_INET && !caglobals.ip.ipv4enabled))
         {
             continue;
         }

resource/csdk/connectivity/src/cainterfacecontroller.c
@@ -484,14 +484,7 @@ CAResult_t CAGetNetworkInfo(CAEndpoint_t **info, size_t *size)
     {
         OICFree(tempInfo);
         OICFree(tempSize);
-        if (res == CA_ADAPTER_NOT_ENABLED || res == CA_NOT_SUPPORTED)
-        {
-            return res;
-        }
-        else
-        {
-            return CA_STATUS_FAILED;
-        }
+        return res;
     }
 
     // #3. add data into result

resource/csdk/connectivity/src/ip_adapter/android/caipnwmonitor.c
@@ -239,12 +239,6 @@ static bool CAParsingNetorkInfo(int idx, u_arraylist_t *iflist)
             continue;
         }
 
-        if ((family == AF_INET6 && !caglobals.ip.ipv6enabled) ||
-            (family == AF_INET && !caglobals.ip.ipv4enabled))
-        {
-            continue;
-        }
-
         int ifindex = if_nametoindex(ifa->ifa_name);
         if (idx && (ifindex != idx))
         {

resource/csdk/connectivity/src/ip_adapter/linux/caipnwmonitor.c
@@ -444,12 +444,6 @@ u_arraylist_t *CAIPGetInterfaceInformation(int desiredIndex)
             continue;
         }
 
-        if ((family == AF_INET6 && !caglobals.ip.ipv6enabled) ||
-            (family == AF_INET && !caglobals.ip.ipv4enabled))
-        {
-            continue;
-        }
-
         CAInterface_t *ifitem = (CAInterface_t *)OICCalloc(1, sizeof(CAInterface_t));
         if (!ifitem)
         {

resource/csdk/connectivity/src/ip_adapter/tizen/caipnwmonitor.c
@@ -283,12 +283,6 @@ static bool CAIPGetAddrInfo(int idx, u_arraylist_t *iflist)
             continue;
         }
 
-        if ((family == AF_INET6 && !caglobals.ip.ipv6enabled) ||
-            (family == AF_INET && !caglobals.ip.ipv4enabled))
-        {
-            continue;
-        }
-
         int ifindex = if_nametoindex(ifa->ifa_name);
         if (idx && (ifindex != idx))
         {

resource/csdk/connectivity/test/ca_api_unittest.cpp
@@ -504,7 +504,7 @@ TEST_F(CATests, HandlerRequestResponseTest)
 }
 
 // CAGetNetworkInformation TC
-TEST_F (CATests, GetNetworkInformationTest)
+TEST_F(CATests, GetNetworkInformationTest)
 {
     size_t tempSize = 0;
     CAEndpoint_t *tempInfo = NULL;
@@ -519,6 +519,71 @@ TEST_F (CATests, GetNetworkInformationTest)
     }
 
     free(tempInfo);
+}
+
+// TODO: This is temporary disabled.
+TEST(CAGetNetworkInformationTest, DISABLED_EnableIPv6)
+{
+    CAInitialize(CA_DEFAULT_ADAPTER);
+    caglobals.ip.ipv6enabled = true;
+    caglobals.ip.ipv4enabled = false;
+
+    size_t tempSize = 0;
+    CAEndpoint_t *tempInfo = NULL;
+
+    EXPECT_EQ(CA_STATUS_OK, CASelectNetwork(CA_ADAPTER_IP));
+    EXPECT_EQ(CA_STATUS_OK, CAGetNetworkInformation(&tempInfo, &tempSize));
+
+    for (size_t index = 0; index < tempSize; index++)
+    {
+        EXPECT_TRUE((tempInfo[index].flags & CA_IPV6) != 0);
+    }
+
+    OICFree(tempInfo);
+    CATerminate();
+}
+
+TEST(CAGetNetworkInformationTest, DISABLED_EnableIPv4)
+{
+    CAInitialize(CA_DEFAULT_ADAPTER);
+    caglobals.ip.ipv6enabled = false;
+    caglobals.ip.ipv4enabled = true;
+
+    size_t tempSize = 0;
+    CAEndpoint_t *tempInfo = NULL;
+
+    EXPECT_EQ(CA_STATUS_OK, CASelectNetwork(CA_ADAPTER_IP));
+    EXPECT_EQ(CA_STATUS_OK, CAGetNetworkInformation(&tempInfo, &tempSize));
+
+    for (size_t index = 0; index < tempSize; index++)
+    {
+        EXPECT_TRUE((tempInfo[index].flags & CA_IPV4) != 0);
+    }
+
+    OICFree(tempInfo);
+    CATerminate();
+}
+
+TEST(CAGetNetworkInformationTest, DISABLED_EnableIPv4AndIPv6)
+{
+    CAInitialize(CA_DEFAULT_ADAPTER);
+    caglobals.ip.ipv6enabled = true;
+    caglobals.ip.ipv4enabled = true;
+
+    size_t tempSize = 0;
+    CAEndpoint_t *tempInfo = NULL;
+
+    EXPECT_EQ(CA_STATUS_OK, CASelectNetwork(CA_ADAPTER_IP));
+    EXPECT_EQ(CA_STATUS_OK, CAGetNetworkInformation(&tempInfo, &tempSize));
+
+    CATransportFlags_t flags = (CATransportFlags_t)(CA_IPV4|CA_IPV6);
+    for (size_t index = 0; index < tempSize; index++)
+    {
+        EXPECT_TRUE((tempInfo[index].flags & flags) != 0 );
+    }
+
+    OICFree(tempInfo);
+    CATerminate();
 }
 
 TEST_F(CATests, GetSelectedNetwork)

commit 81df764e7ade13fdc74ecd7d50148a809291752c

IOT-2351 CA: Parse all interfaces
The local variable interfaces gets decremented during the loop,
but the entire iflist has to be parsed.

Fixes new test errors that started on my machine with recent change
e3a3f65569f15fdf711c9d0a3123e6951f6499ba :

[ RUN      ] CATests.GetNetworkInformationTest
resource\csdk\connectivity\test\ca_api_unittest.cpp(517): error:
Value of: tempInfo[index].adapter != 0
  Actual: false
Expected: true
resource\csdk\connectivity\test\ca_api_unittest.cpp(518): error:
Value of: strlen(tempInfo[index].addr) != 0
  Actual: false
Expected: true
resource\csdk\connectivity\test\ca_api_unittest.cpp(517): error:
Value of: tempInfo[index].adapter != 0
  Actual: false
Expected: true
resource\csdk\connectivity\test\ca_api_unittest.cpp(518): error:
Value of: strlen(tempInfo[index].addr) != 0
  Actual: false
Expected: true
resource\csdk\connectivity\test\ca_api_unittest.cpp(517): error:
Value of: tempInfo[index].adapter != 0
  Actual: false
Expected: true
resource\csdk\connectivity\test\ca_api_unittest.cpp(518): error:
Value of: strlen(tempInfo[index].addr) != 0
  Actual: false
Expected: true
resource\csdk\connectivity\test\ca_api_unittest.cpp(517): error: Value of:
tempInfo[index].adapter != 0
  Actual: false
Expected: true
resource\csdk\connectivity\test\ca_api_unittest.cpp(518): error: Value of:
strlen(tempInfo[index].addr) != 0
  Actual: false
Expected: true
[  FAILED  ] CATests.GetNetworkInformationTest (5 ms)

Change-Id: If3a74a0739be6a53373f8b25436a50304029d751
Signed-off-by: Dan Mihai <Daniel.Mihai@microsoft.com>
Reviewed-on: https://gerrit.iotivity.org/gerrit/20337
Reviewed-by: Jaehong Jo <jaehong.jo@samsung.com>
Tested-by: jenkins-iotivity <jenkins@iotivity.org>
Reviewed-by: Uze Choi <uzchoi@samsung.com>

resource/csdk/connectivity/src/ip_adapter/caipserver.c
@@ -1543,7 +1543,7 @@ CAResult_t CAGetIPInterfaceInformation(CAEndpoint_t **info, size_t *size)
 #endif
 
     size_t interfaces = u_arraylist_length(iflist);
-    for (size_t i = 0; i < interfaces; i++)
+    for (size_t i = 0; i < u_arraylist_length(iflist); i++)
     {
         CAInterface_t *ifitem = (CAInterface_t *)u_arraylist_get(iflist, i);
         if (!ifitem)

resource/csdk/connectivity/src/tcp_adapter/catcpserver.c
@@ -1320,7 +1320,7 @@ CAResult_t CAGetTCPInterfaceInformation(CAEndpoint_t **info, size_t *size)
 #endif
 
     size_t interfaces = u_arraylist_length(iflist);
-    for (size_t i = 0; i < interfaces; i++)
+    for (size_t i = 0; i < u_arraylist_length(iflist); i++)
     {
         CAInterface_t *ifitem = (CAInterface_t *)u_arraylist_get(iflist, i);
         if (!ifitem)