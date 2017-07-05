66adeba406c1b58cb288a57659f91041c200057d
hyuna0213.jo <hyuna0213.jo@samsung.com>
2017-07-05 15:56:20 +0900

    Implement light sample without extension
    LightServerSample has binaryswitch resource.
    
    Change-Id: I581c889dcae04e40087ca9c120181b3d4f54df43
    Signed-off-by: Glen Youngjin Kim <glen.kim@samsung.com>
    Signed-off-by: hyuna0213.jo <hyuna0213.jo@samsung.com>
    Reviewed-on: https://gerrit.iotivity.org/gerrit/19813
    Tested-by: jenkins-iotivity <jenkins@iotivity.org>
    Reviewed-by: Jaewook Jung <jw0213.jung@samsung.com>
    Reviewed-by: JungYong KIM <jyong2.kim@samsung.com>

--- service/smarthome-api/examples/LightServerSample.cpp
+++ service/smarthome-api/examples/LightServerSample.cpp
@@ -18,59 +18,21 @@
  *
  ******************************************************************/
 #include <iostream>
-#include <pthread.h>
-#include <cstdlib>
-#include <signal.h>
-#include "octhread.h"
-#include "LightServerSample.h"
+#include <CommonApi.h>
+#include <LightDevice.h>
+#include <ResourceQuery.h>
 
 using namespace std;
-
-#define USER_RESOURCE_URI "/color"
-#define USER_RESOURCE_TYPE "core.color"
-#define USER_RESOURCE_PROP_KEY "color"
-
-static oc_mutex g_mutex = NULL;
-static oc_cond g_cond = NULL;
-
 using namespace OIC::Service::SH;
 
-UserColorResource::UserColorResource() :
-        SHBaseResource(USER_RESOURCE_URI, USER_RESOURCE_TYPE)
+class MyBinarySwitchDelegate: public BinarySwitchResourceDelegate
 {
-    std::cout << "[UserColorResource] constructor" << std::endl;
-
-    std::string color = "black";
-    PropertyBundle bundle;
-    bundle.setValue(USER_RESOURCE_PROP_KEY, color);
-    setPropertyBundle(bundle);
-}
-
-UserColorResource::~UserColorResource()
-{
-}
-
-void UserColorResource::setColor(std::string color)
-{
-    PropertyBundle bundle;
-    bundle.setValue(USER_RESOURCE_PROP_KEY, color);
-    setPropertyBundle(bundle);
-}
-
-std::string UserColorResource::getColor()
-{
-    PropertyBundle bundle = getPropertyBundle();
-
-    // Get "color" property value from bundle.
-    std::string color;
-    bundle.getValue(USER_RESOURCE_PROP_KEY, color);
-    return color;
-}
-
-void UserColorResource::setColorDelegate(MyColorDelegate* delegate)
-{
-    setDelegate(delegate);
-}
+public:
+    virtual ~MyBinarySwitchDelegate() {};
+public:
+    virtual ResultCode turnOnCallback();
+    virtual ResultCode turnOffCallback();
+};
 
 ResultCode MyBinarySwitchDelegate::turnOnCallback()
 {
@@ -84,122 +46,62 @@ ResultCode MyBinarySwitchDelegate::turnOffCallback()
     return SUCCESS;
 }
 
-ResultCode MyColorDelegate::onGet(RequestId requestId, const ResourceQuery& query)
+void print()
 {
-    std::cout << "[LightServerSample][Color] onGet.." << endl;
-    return SUCCESS;
-}
-
-ResultCode MyColorDelegate::onSet(RequestId requestId, const PropertyBundle& bundle,
-                                  const ResourceQuery& query)
-{
-    std::cout << "[LightServerSample][Color] onSet.." << endl;
-    return SUCCESS;
-}
-
-void PrintUsage()
-{
-    std::cout << std::endl;
-    std::cout << "Usage : LightServerSample <value>\n";
-    std::cout << "    1 - pre-defined light device(binaryswitch resource)\n\n";
-    std::cout << "    2 - extended light device(binaryswitch + color resource)\n";
-}
-
-/* SIGINT handler: set gQuitFlag to 1 for graceful termination */
-void handleSigInt(int signum)
-{
-    if (signum == SIGINT)
-    {
-        oc_mutex_lock(g_mutex);
-        oc_cond_signal(g_cond);
-        oc_mutex_unlock(g_mutex);
-    }
+    std::cout << "==============================================================" << std::endl;
+    std::cout << "0. print menu" << std::endl;
+    std::cout << "1. turn on switch" << std::endl;
+    std::cout << "2. turn off switch" << std::endl;
+    std::cout << "9. Quit" << std::endl;
+    std::cout << "===============================================================" << std::endl;
+    std::cout << "cmd>";
 }
 
 static LightDevice *myLight = NULL;
-static UserLight *userLight = NULL;
-
 static MyBinarySwitchDelegate *switchDelegate = NULL;
-static MyColorDelegate *colorDelegate = NULL;
 
-int main(int argc, char* argv[])
+int main()
 {
-    PrintUsage();
+    std::cout << "#1. Create Predefined Light Device" << endl;
+    myLight = new LightDevice;
+    std::cout << "#2. Set MyBinarySwitchDelegate" << endl;
+    switchDelegate = new MyBinarySwitchDelegate;
+    myLight->m_binarySwitch.setDelegate(switchDelegate);
 
-    bool isPredefined = true;
-    if (argc == 2)
+    bool quitflag = false;
+    int cmd;
+    print();
+    while (!quitflag)
     {
-        int value = atoi(argv[1]);
-        switch (value)
+        std::cin >> cmd;
+
+        if (0 == cmd)
         {
-            case 1:
-                isPredefined = true;
-                break;
-            case 2:
-                isPredefined = false;
-                break;
-            default:
-                break;
+            print();
+        }
+        else if (1 == cmd)
+        {
+            myLight->m_binarySwitch.setState(true);
+        }
+        else if (2 == cmd)
+        {
+            myLight->m_binarySwitch.setState(false);
+        }
+        else if (9 == cmd)
+        {
+            quitflag = true;
+        }
+        else
+        {
+            std::cout << "Unknown cmd." << std::endl;
+            cmd = 0;
+            std::cin.clear();
+            std::cin.ignore(1024, '\n');
         }
     }
-    else
-    {
-        return -1;
-    }
 
-    if (isPredefined)
-    {
-        std::cout << "======================================================" << endl;
-        std::cout << "#1. Create Predefined Light Device" << endl;
-        myLight = new LightDevice;
-        std::cout << "#2. Set MyBinarySwitchDelegate" << endl;
-        switchDelegate = new MyBinarySwitchDelegate;
-        myLight->m_binarySwitch.setDelegate(switchDelegate);
-
-        myLight->m_binarySwitch.getState();
-        std::cout << "======================================================" << endl;
-
-    }
-    else
-    {
-        std::cout << "======================================================" << endl;
-        std::cout << "#3. Create Extended Light Device" << endl;
-        userLight = new UserLight;
-
-        std::cout << "#4. Set MyBinarySwitchDelegate" << endl;
-        switchDelegate = new MyBinarySwitchDelegate;
-        userLight->m_binarySwitch.setDelegate(switchDelegate);
-
-        std::cout << "#5. Set MyColorDelegate" << endl;
-        colorDelegate = new MyColorDelegate;
-        userLight->color.setColorDelegate(colorDelegate);
-        std::cout << "======================================================" << endl;
-    }
-
-    g_cond = oc_cond_new();
-    g_mutex = oc_mutex_new();
-
-    signal(SIGINT, handleSigInt);
-
-    oc_mutex_lock(g_mutex);
-    oc_cond_wait(g_cond, g_mutex);
-    signal(SIGINT, SIG_IGN);
-    oc_mutex_unlock(g_mutex);
-
-    if (isPredefined)
-    {
-        delete (myLight);
-        delete (switchDelegate);
-    }
-    else
-    {
-        delete (userLight);
-        delete (switchDelegate);
-        delete (colorDelegate);
-    }
-
-    oc_mutex_free(g_mutex);
-    oc_cond_free(g_cond);
+    delete (myLight);
+    delete (switchDelegate);
 
     return 0;
 }
