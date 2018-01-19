ac15361434243c5b3f2c570d87f2b37f457a1da0
George Nash <george.nash@intel.com>
2018-01-19 15:30:31 -0800

    Clean build warnings
    This cleans new build warnings discovered when building
    iotivity with GCC 7.2.1
    
    -Werror=format-truncation
    Fixed in json2cbor that was a result of incorrect buffer size in snprintf
    
    -Werror=implicit-fallthrough
    With the addition of the [[fallthrough]] keyword in C++17 GCC now detects
    fallthrough locations. The fallthrough error can be hidden by adding
    the comment "fall through" to the case that is falling though.
    
    -Werror=pragmas and -Werror=attributes
    are from the external tinyCbor code. We Ignore build warnings from extern
    libraries. We may fix the warnings by working witht he external project
    but it should not block any work in IoTivity.
    
    Change-Id: I86c9fb42b987858c66698fe48e4c2a405769b004
    Signed-off-by: George Nash <george.nash@intel.com>

--- resource/csdk/security/tool/json2cbor.c
+++ resource/csdk/security/tool/json2cbor.c
@@ -1268,7 +1268,7 @@ static OicSecCred_t *JSONToCredBinWithRowner(const char *jsonStr,OicUuid_t *rown
                 VERIFY_NOT_NULL(TAG, buf, ERROR);
                 for(size_t i = 0, p = 0 ; i < jsonObjLen; i+=2, ++p)
                 {
-                    snprintf(tmp, 2, "%c%c", jsonPriv->valuestring[i], jsonPriv->valuestring[i+1]);
+                    snprintf(tmp, sizeof(tmp), "%c%c", jsonPriv->valuestring[i], jsonPriv->valuestring[i+1]);
                     buf[p] = (char)strtol(tmp, NULL, 16);
                 }
                 cred->privateData.len = jsonObjLen/2;
@@ -1297,7 +1297,7 @@ static OicSecCred_t *JSONToCredBinWithRowner(const char *jsonStr,OicUuid_t *rown
                 VERIFY_NOT_NULL(TAG, buf, ERROR);
                 for(size_t i = 0, p = 0 ; i < jsonObjLen; i+=2, ++p)
                 {
-                    snprintf(tmp, 2, "%c%c", jsonPub->valuestring[i], jsonPub->valuestring[i+1]);
+                    snprintf(tmp, sizeof(tmp), "%c%c", jsonPub->valuestring[i], jsonPub->valuestring[i+1]);
                     buf[p] = (char)strtol(tmp, NULL, 16);
                 }
                 cred->publicData.len = jsonObjLen/2;
@@ -1326,7 +1326,7 @@ static OicSecCred_t *JSONToCredBinWithRowner(const char *jsonStr,OicUuid_t *rown
                 VERIFY_NOT_NULL(TAG, buf, ERROR);
                 for(size_t i = 0, p = 0; i < jsonObjLen; i+=2, ++p)
                 {
-                    snprintf(tmp, 2, "%c%c", jsonOpt->valuestring[i], jsonOpt->valuestring[i+1]);
+                    snprintf(tmp, sizeof(tmp), "%c%c", jsonOpt->valuestring[i], jsonOpt->valuestring[i+1]);
                     buf[p] = (char)strtol(tmp, NULL, 16);
                 }
                 cred->optionalData.len = jsonObjLen/2;
