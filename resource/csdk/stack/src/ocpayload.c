1e78108a820077a709314efa8eb87aba430e3abe
uzchoi <uzchoi@samsung.com>
2018-02-05 17:31:58 +0900

    [IOT-2762] CreateEndpoint err in secured mode
    In case of server process and client process exist in a same machine,
    ifindex of CAEndpoint_t is different from the ifindex of Request DevAddr
    because the ifindex of request DevAddr indicate local loopback in 
    secured mode while CAEndpoint_t shows physical interface.
    This error can be fixed by checking whether the sender is from loopback.
    However, some machine shows wrong print in if_indextoname() such as 
    eth0 instead of lo.
    For this reason, I removed the comparison of ifindex. For more exact 
    check, we can put IP address is from my machine but looks inefficient.
    
    Change-Id: Ic67fd2b4609df5779cb2775f632dca79b2bc7232
    Signed-off-by: uzchoi <uzchoi@samsung.com>

--- resource/csdk/stack/src/ocpayload.c
+++ resource/csdk/stack/src/ocpayload.c
@@ -1825,10 +1825,11 @@ OCEndpointPayload* CreateEndpointPayloadList(const OCResource *resource, const O
         for (size_t i = 0; i < infoSize; i++)
         {
             CAEndpoint_t *info = networkInfo + i;
+            OIC_LOG_V(DEBUG, TAG, "CATransportAdapter_t value = %d", info->adapter);
+            OIC_LOG_V(DEBUG, TAG, "info[%d], info->ifindex = %d, devAddr->ifindex = %d"
+                                  , (int)i, info->ifindex, devAddr->ifindex);
 
-            if (((CA_ADAPTER_IP | CA_ADAPTER_TCP) & info->adapter &&
-                 info->ifindex == devAddr->ifindex) ||
-                info->adapter == CA_ADAPTER_RFCOMM_BTEDR)
+            if ((CA_ADAPTER_IP | CA_ADAPTER_TCP) & info->adapter)
             {
                 OCTpsSchemeFlags matchedTps = OC_NO_TPS;
                 if (OC_STACK_OK != OCGetMatchedTpsFlags(info->adapter,
@@ -1855,7 +1856,7 @@ OCEndpointPayload* CreateEndpointPayloadList(const OCResource *resource, const O
                     OCStackResult ret = OCConvertTpsToString(matchedTps, &(tmpNode->tps));
                     if (ret != OC_STACK_OK)
                     {
-                        OIC_LOG_V(DEBUG, TAG, "OCConvertTpsToString(%s) is false", tmpNode->tps);
+                        OIC_LOG_V(ERROR, TAG, "OCConvertTpsToString(%s) is false", tmpNode->tps);
                         OCDiscoveryEndpointDestroy(tmpNode);
                         goto exit;
                     }
@@ -1899,10 +1900,6 @@ OCEndpointPayload* CreateEndpointPayloadList(const OCResource *resource, const O
                         (*epSize)++;
                     }
                 }
-            }
-            else
-            {
-                OIC_LOG_V(DEBUG, TAG, "CATransportAdapter_t value = %d", info->adapter);
             }
         }
     }
