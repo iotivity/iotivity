e7ff91f55d01e288b938095c95eca71c7caa59ee
Aleksey Volkov <a.volkov@samsung.com>
2018-12-19 18:19:43 +0200

    [IOT-3150] Set doxm.owned as read-only
    BZ 2692 - "owned" of "/oic/sec/doxm"
    should be set by Device itself
    
    - Set access mode to Read-Only
    - Set value to TRUE, when the device switches to RFPRO, RFNOP
    - Set value to FALSE, when the device switches to RESET, RFOTM
    
    Change-Id: I0dd9112150fb2f20e4bdda90d56c9352ccdac0d8
    Signed-off-by: Aleksey Volkov <a.volkov@samsung.com>

--- resource/csdk/security/src/doxmresource.c
+++ resource/csdk/security/src/doxmresource.c
@@ -111,7 +111,7 @@ static const uint8_t gDoxmPropertyAccessModes[DOXM_PROPERTY_COUNT][DOS_STATE_COU
     { R,    RW,     R,      R,      R   }, // .oxmsel
 #endif // MULTIPLE_OWNER
     { R,    R,      R,      R,      R   }, // .sct
-    { R,    RW,     R,      R,      R   }, // .owned
+    { R,    R,      R,      R,      R   }, // .owned
 #ifdef MULTIPLE_OWNER
     { RW,   RW,    RW,      RW,     RW  }, // .subowner
     { RW,   RW,    RW,      RW,     RW  }, // .mom
